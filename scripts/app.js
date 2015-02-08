c8afbc224fefc12b3c11eeb6dbe0c1e2261d50de
Ozzie Gooen <ozzieagooen@gmail.com>
2015-02-07 22:42:48 -0800

    basic template

--- scripts/app.js
+++ scripts/app.js
@@ -5,7 +5,7 @@ var React = require('react');
 var App = React.createClass({
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <h1>Hello, world!</h1>
     );
   }
 });
---
+++ scripts/enode.js
@@ -0,0 +1,10 @@
+'use strict';
+
+class Enode {
+  constructor(properties){
+    this.properties = properties;
+  }
+}
+
+
+module.exports = Enode;
