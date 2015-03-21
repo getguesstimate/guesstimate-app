011b90c610a28805fa8141f53d9cc75d56aa7846
Ozzie Gooen <ozzieagooen@gmail.com>
2015-03-20 23:39:29 -0700

    beginning to move files around

---
+++ scripts/operations.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var operations = {
+  'multiplication': {
+    name: 'multiplication',
+    sign: 'x',
+    apply(inputs){
+      var product = _.reduce(inputs, function(product, n) { return product * n; })
+      return product;
+    }
+  },
+  'addition': {
+    name: 'addition',
+    sign: '+',
+    apply(inputs){
+      var sum = _.reduce(inputs, function(sum, n) { return sum + n; });
+      return sum;
+    }
+  }
+}
+
+module.exports = operations;
