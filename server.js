c7880782a0f179d9bdb314e8c8c560c9b58eff44
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-03-29 14:52:42 -0700

    added express

--- server.js
+++ server.js
@@ -1,7 +1,35 @@
 var webpack = require('webpack');
-
 var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
+
+var express    = require('express');        // call express
+var bodyParser = require('body-parser');
+var app        = express();                 // define our app using express
+var mongoose = require('mongoose');
+var router = express.Router();              // get an instance of the express Router
+mongoose.connect('mongodb://oagr:password@proximus.modulusmongo.net:27017/tas3uZuj');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+var comments = [{author: 'Pete Hunt', text: 'Hey there!'}];
+
+app.all('/', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+  console.log(res)
+  next();
+});
+
+app.get('/comments.json', function(req, res) {
+   res.setHeader('Content-Type', 'application/json');
+   res.send(JSON.stringify(comments));
+});
+
+app.post('/comments.json', function(req, res) {
+   res.send(JSON.stringify(comments));
+ });
+
+app.listen(3001);
 
 new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
