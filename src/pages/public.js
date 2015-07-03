bffa9fafd3805b303243487d3a6762b570f4473a
Ozzie Gooen <ozzieagooen@gmail.com>
2015-07-02 23:38:01 -0700

    Basic page working

---
+++ src/pages/public.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default React.createClass({
+  displayName: 'PublicPage',
+  render () {
+    return (
+      <div className='container'>
+        <header role='banner'>
+          <h1>Labelr</h1>
+        </header>
+        <div>
+          <p>We label stuff for you, because, we can&trade;</p>
+          <a href='/repos' className='button button-large'>
+            <span className='mega-octicon octicon-mark-github'></span> Login with GitHub
+          </a>
+        </div>
+      </div>
+    )
+  }
+})
+
