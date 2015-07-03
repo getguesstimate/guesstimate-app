ee42e8792f8c4cf0b8216a92f3c235b489c13a5d
Ozzie Gooen <ozzieagooen@gmail.com>
2015-07-03 16:21:14 -0700

    Integrated cytoscape to work with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,14 @@
 var getConfig = require('hjs-webpack')
+var webpack = require('webpack')
 
 module.exports = getConfig({
   in: 'src/app.js',
   out: 'public',
   clearBeforeBuild: true,
   hostName: 'ozzie-Elitebook.local'
-})
+});
+
+module.exports.node = {
+  child_process: 'empty'
+}
+
