e1341ae5d323c0da4868303d386d103a87f32cf0
Ozzie Gooen <ozzieagooen@gmail.com>
2015-07-03 21:05:58 -0700

    Removed uneccessary pages, added home page

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import app from 'ampersand-app'
 import Router from './router'
-import styles from './css/main.css'
+import styles from './css/main.styl'
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.css"
 
 window.app = app
