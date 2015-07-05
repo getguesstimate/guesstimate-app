3f6fe7b5bddc3ca6b02b432effe41bf67a138190
Ozzie Gooen <ozzieagooen@gmail.com>
2015-07-05 00:26:18 -0700

    starting integration

--- src/app.js
+++ src/app.js
@@ -3,11 +3,14 @@ import Router from './router'
 import styles from './css/main.styl'
 import bootstrap from "../node_modules/bootstrap/dist/css/bootstrap.css"
 import Me from './models/me'
+import Firebase from 'firebase'
 
 window.app = app
 
 app.extend({
   init () {
+    this.firebase = new Firebase("https://brilliant-inferno-9726.firebaseio.com/")
+    this.firebase.child('repos').on('value', (snapshot) => { this.allRepos = snapshot.val()})
     this.me = new Me()
     this.router = new Router()
     this.router.history.start()
--- src/models/repo-collection.js
+++ src/models/repo-collection.js
@@ -5,64 +5,67 @@ export default Collection.extend({
   model: Repo,
 
   initialize () {
-    this.add([
-      {
-        name: 'people-in-nyc',
-        data: {
-          nodes: [
-            {pid: 11, nodeType: 'estimate', name: 'people in NYC', value: 10000000},
-            {pid: 2, nodeType: 'estimate', name: 'families per person', value: 0.3},
-            {pid: 3, nodeType: 'estimate', name: 'pianos per family', value: 0.1},
-            {pid: 4, nodeType: 'estimate', name: 'piano tuners per family', value: 0.001},
-            {pid: 5, nodeType: 'dependent', name: 'families in NYC'},
-            {pid: 6, nodeType: 'dependent', name: 'pianos in NYC'},
-            {pid: 7, nodeType: 'dependent', name: 'piano tuners in NYC'},
-            {pid: 8, nodeType: 'function', functionType: 'multiplication'},
-            {pid: 9, nodeType: 'function', functionType: 'multiplication'},
-            {pid: 10, nodeType: 'function', functionType: 'multiplication'},
-          ],
-          edges: [
-            [11,8],
-            [2,8],
-            [8,5],
-            [5,9],
-            [3,9],
-            [9,6],
-            [6,10],
-            [4,10],
-            [10,7],
-          ]
-        }
-      },
-      {
-        name: 'people-in-suburbs',
-        data: {
-          nodes: [
-            {pid: 11, nodeType: 'estimate', name: 'people in Suburbs', value: 10000000},
-            {pid: 2, nodeType: 'estimate', name: 'families per person', value: 0.3},
-            {pid: 3, nodeType: 'estimate', name: 'pianos per family', value: 0.1},
-            {pid: 4, nodeType: 'estimate', name: 'piano tuners per family', value: 0.001},
-            {pid: 5, nodeType: 'dependent', name: 'families in Suburbs'},
-            {pid: 6, nodeType: 'dependent', name: 'pianos in Suburbs'},
-            {pid: 7, nodeType: 'dependent', name: 'piano tuners in Suburbs'},
-            {pid: 8, nodeType: 'function', functionType: 'multiplication'},
-            {pid: 9, nodeType: 'function', functionType: 'multiplication'},
-            {pid: 10, nodeType: 'function', functionType: 'multiplication'},
-          ],
-          edges: [
-            [11,8],
-            [2,8],
-            [8,5],
-            [5,9],
-            [3,9],
-            [9,6],
-            [6,10],
-            [4,10],
-            [10,7],
-          ]
-        }
-      }
-    ])
+    //const foobar = [
+      //{
+        //name: 'people-in-nyc',
+        //data: {
+          //nodes: [
+            //{pid: 11, nodeType: 'estimate', name: 'people in NYC', value: 10000000},
+            //{pid: 2, nodeType: 'estimate', name: 'families per person', value: 0.3},
+            //{pid: 3, nodeType: 'estimate', name: 'pianos per family', value: 0.1},
+            //{pid: 4, nodeType: 'estimate', name: 'piano tuners per family', value: 0.001},
+            //{pid: 5, nodeType: 'dependent', name: 'families in NYC'},
+            //{pid: 6, nodeType: 'dependent', name: 'pianos in NYC'},
+            //{pid: 7, nodeType: 'dependent', name: 'piano tuners in NYC'},
+            //{pid: 8, nodeType: 'function', functionType: 'multiplication'},
+            //{pid: 9, nodeType: 'function', functionType: 'multiplication'},
+            //{pid: 10, nodeType: 'function', functionType: 'multiplication'},
+          //],
+          //edges: [
+            //[11,8],
+            //[2,8],
+            //[8,5],
+            //[5,9],
+            //[3,9],
+            //[9,6],
+            //[6,10],
+            //[4,10],
+            //[10,7],
+          //]
+        //}
+      //},
+      //{
+        //name: 'people-in-suburbs',
+        //data: {
+          //nodes: [
+            //{pid: 11, nodeType: 'estimate', name: 'people in Suburbs', value: 10000000},
+            //{pid: 2, nodeType: 'estimate', name: 'families per person', value: 0.3},
+            //{pid: 3, nodeType: 'estimate', name: 'pianos per family', value: 0.1},
+            //{pid: 4, nodeType: 'estimate', name: 'piano tuners per family', value: 0.001},
+            //{pid: 5, nodeType: 'dependent', name: 'families in Suburbs'},
+            //{pid: 6, nodeType: 'dependent', name: 'pianos in Suburbs'},
+            //{pid: 7, nodeType: 'dependent', name: 'piano tuners in Suburbs'},
+            //{pid: 8, nodeType: 'function', functionType: 'multiplication'},
+            //{pid: 9, nodeType: 'function', functionType: 'multiplication'},
+            //{pid: 10, nodeType: 'function', functionType: 'multiplication'},
+          //],
+          //edges: [
+            //[11,8],
+            //[2,8],
+            //[8,5],
+            //[5,9],
+            //[3,9],
+            //[9,6],
+            //[6,10],
+            //[4,10],
+            //[10,7],
+          //]
+        //}
+      //}
+    //]
+    //app.firebase.child("repos").push(foobar[0])
+    //app.firebase.child("repos").push(foobar[1])
+    this.add(foobar)
   },
 
   getByName (name) {
