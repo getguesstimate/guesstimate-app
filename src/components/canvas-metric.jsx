810cc6b6dcc5f317222abe251465312a2430b5e0
Ozzie Gooen <ozzieagooen@gmail.com>
2015-08-28 00:02:52 -0700

    Using card format

--- src/components/canvas-metric.jsx
+++ src/components/canvas-metric.jsx
@@ -3,20 +3,27 @@ import SpaceActions from '../actions/space-actions'
 import Button from 'react-bootstrap/lib/Button'
 import Guesstimate from './canvas-guesstimate'
 import Distribution from './canvas-distribution'
+import Panel from 'react-bootstrap/lib/Panel'
 
 const MetricWidget = React.createClass({
   propogate() {
     SpaceActions.metricPropogate(this.props.metric.id)
   },
   render() {
+    let foo = <Guesstimate guesstimate={this.props.metric.guesstimates[0]} metricId={this.props.metric.id}/>
     return (
     <div className="col-sm-3 metric">
-      <h2>{this.props.metric.name}</h2>
-      <div className="node-form">
-        <Distribution distribution={this.props.metric.distribution()} metricId={this.props.metric.id}/>
-        <Guesstimate guesstimate={this.props.metric.guesstimates[0]} metricId={this.props.metric.id}/>
-        <Button bsSize='xsmall' onClick={this.propogate}>Propogate!</Button>
+      <Panel footer={foo}>
+      <div className="row">
+        <div className="col-sm-6">
+          <Distribution distribution={this.props.metric.distribution()} metricId={this.props.metric.id}/>
+        </div>
+        <div className="col-sm-6">
+          <h2>{this.props.metric.name}</h2>
+          <Button bsSize='xsmall' onClick={this.propogate}>Propogate!</Button>
+        </div>
       </div>
+      </Panel>
     </div>
     )
   }
