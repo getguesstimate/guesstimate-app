700503647da842c546480a83cb8714fa98596dc3
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-08-28 00:31:57 -0700

    Put stats in special tables, moved histogram lower

--- src/components/canvas-metric.jsx
+++ src/components/canvas-metric.jsx
@@ -4,6 +4,7 @@ import Button from 'react-bootstrap/lib/Button'
 import Guesstimate from './canvas-guesstimate'
 import Distribution from './canvas-distribution'
 import Panel from 'react-bootstrap/lib/Panel'
+import Icon from'react-fa'
 
 const MetricWidget = React.createClass({
   propogate() {
@@ -15,12 +16,12 @@ const MetricWidget = React.createClass({
     <div className="col-sm-3 metric">
       <Panel footer={foo}>
       <div className="row">
-        <div className="col-sm-6">
+        <div className="col-sm-7">
           <Distribution distribution={this.props.metric.distribution()} metricId={this.props.metric.id}/>
         </div>
-        <div className="col-sm-6">
+        <div className="col-sm-5">
           <h2>{this.props.metric.name}</h2>
-          <Button bsSize='xsmall' onClick={this.propogate}>Propogate!</Button>
+          <Button bsSize='xsmall' onClick={this.propogate}><Icon name='refresh'/></Button>
         </div>
       </div>
       </Panel>
