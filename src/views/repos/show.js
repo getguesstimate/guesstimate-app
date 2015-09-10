0c98bf2adb5b4fc0a165e8bfbae1a16f5cb0b480
Ozzie Gooen <ozzieagooen@gmail.com>
2015-09-09 19:16:22 -0700

    Removed further cytoscape code

--- src/views/repos/show.js
+++ src/views/repos/show.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import GraphEditor from 'components/canvas-space'
+import SpaceCanvas from 'components/canvas-space'
 import ampersandMixin from 'ampersand-react-mixin'
 import Icon from'react-fa'
 
@@ -10,14 +10,9 @@ export default React.createClass({
 
   render () {
     let repo = <Icon spin name="spinner"/>
-    if (app.me.repos.models.length != 0) {
+    if (app.me.repos.models.length !== 0) {
       let rep = app.me.repos.getByName(this.props.repo)
-      repo = <GraphEditor repo={rep} savable={true} key={this.props.repo} />
     }
-    return (
-      <div className='repo-page'>
-        {repo}
-      </div>
-    )
+    return (<SpaceCanvas/>)
   }
 })
