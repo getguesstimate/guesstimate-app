50172ee54551671c1c0a683f515253edee2bd14a
Ozzie Gooen <ozzieagooen@gmail.com>
2015-09-20 13:15:59 -0700

    Grid auto expands downwards

--- src/components/canvas-space/canvas-space.jsx
+++ src/components/canvas-space/canvas-space.jsx
@@ -37,11 +37,19 @@ export default class CanvasSpace extends Component{
   _handleAddMetric(location) {
     this.props.dispatch(addMetric(location))
   }
+  size(){
+    const lowest_metric = Math.max(...this.props.metrics.map(g => g.location.row)) + 2
+    const selected = this.props.selected.row + 2
+    const height = Math.max(3, lowest_metric, selected) || 3;
+    return {columns: 4, rows: height}
+  }
   render () {
     let guesstimate = (m) => { return this.props.guesstimates.filter((g) => { return g.metric === m })[0]}
+    let size = this.size()
+    console.log(size)
     return (
       <div className="canvas-space">
-        <Grid selected={this.props.selected} handleSelect={this._handleSelect.bind(this)} onAddItem={this._handleAddMetric.bind(this)}>
+        <Grid size={size} selected={this.props.selected} handleSelect={this._handleSelect.bind(this)} onAddItem={this._handleAddMetric.bind(this)}>
           {
             this.props.metrics.map((m) => {
               return (<Metric metric={m} guesstimate={guesstimate(m.id)} key={m.id} location={m.location} canvasState={this.props.canvasState}/>)
