d4ac00be6ff7adce4d002e97ebc91ef8f8485f1d
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-09-21 23:09:10 -0700

    Added +- to indicate uncertainty

--- src/models/guesstimate-form.js
+++ src/models/guesstimate-form.js
@@ -11,21 +11,27 @@ export class EstimateForm{
     if (this.state.includes('/')){
       let [mean, stdev] = this.state.split('/').map((e) => parseFloat(e.trim()));
       return {mean, stdev};
+    } else if (this.state.includes('+-')){
+      let [mean, stdev] = this.state.split('+-').map((e) => parseFloat(e.trim()));
+      return {mean, stdev};
+    } else if (this.state.includes('-+')){
+      let [mean, stdev] = this.state.split('+-').map((e) => parseFloat(e.trim()));
+      return {mean, stdev};
     } else if (this.state.includes('->')){
       let [low, high] = this.state.split('->').map((e) => parseFloat(e.trim()));
       if (parseFloat(high) > parseFloat(low)){
-        let mean = low + ((high - low) / 2)
-        let stdev = (high-mean)
+        let mean = low + ((high - low) / 2);
+        let stdev = (high-mean);
         return {mean, stdev};
       } else {
-        return {mean: null, stdev:null, errors: ['Estimate: [low]->[high]: High must be greater than low. ']}
+        return {mean: null, stdev:null, errors: ['Estimate: [low]->[high]: High must be greater than low. ']};
       }
     } else if (parseFloat(this.state.toString()).toString() === this.state){
       return {mean: this.state, stdev: 0};
     } else if (this.state == ''){
       return {mean: null, stdev: null};
     } else {
-      return {mean: null, stdev:0, errors: ['Estimate: Could not parse. Use "/" or "->" symbols.']}
+      return {mean: null, stdev:0, errors: ['Estimate: Could not parse. Use "/" or "->" symbols.']};
     }
   }
 }
