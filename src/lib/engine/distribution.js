4ec1af6f7eb36b66436980f51dc38d603e63e693
Ozzie Gooen <ozzieagooen@gmail.com>
2015-09-28 15:06:32 -0700

    Adding flow support

--- src/lib/engine/distribution.js
+++ src/lib/engine/distribution.js
@@ -1,8 +1,14 @@
+/* @flow */
+
 import _ from 'lodash'
 import Stochator from 'stochator';
 import * as functionDistribution from './functionDistribution.js';
 
-export function sample(distribution, dGraph, n=1){
+type Distribution = {mean: number; stdev: number, input: string};
+type DGraph = {metrics: any};
+type Sample = {values?: Array<number>, errors?: Array<Object>};
+
+export function sample(distribution: Distribution, dGraph: DGraph, n: number = 1): Sample {
   if (isNormal(distribution)){
     return sampleNormal(distribution, n)
   } else {
@@ -11,17 +17,17 @@ export function sample(distribution, dGraph, n=1){
   }
 }
 
-function isNormal(distribution){
+function isNormal(distribution: Distribution): boolean{
   return (!_.isUndefined(distribution.mean) && !_.isUndefined(distribution.stdev));
 }
 
-function sampleNormal(distribution, n=1){
-    let stochator = new Stochator({
-      mean: distribution.mean,
-      stdev: distribution.stdev,
-      seed: 0,
-      min: 0
-    });
-    //This makes the outputs integers.  This could change, of course
-    return { values: stochator.next(n).map(n => Math.floor(n)) }
+function sampleNormal(distribution: Distribution, n: number = 1){
+  let stochator = new Stochator({
+    mean: distribution.mean,
+    stdev: distribution.stdev,
+    seed: 0,
+    min: 0
+  });
+  //This makes the outputs integers.  This could change, of course
+  return { values: stochator.next(n).map(n => Math.floor(n)) }
 }
