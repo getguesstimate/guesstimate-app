bbf1909e9c057af332e7a2ca9e3f0b78d727a520
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-10-08 00:14:02 -0700

    Metric Name edit attempt 2, with only textarea

--- src/components/metrics/card/name/index.js
+++ src/components/metrics/card/name/index.js
@@ -4,71 +4,42 @@ import ReactDOM from 'react-dom'
 import _ from 'lodash'
 import './style.css'
 
-class MetricForm extends Component {
-  componentDidMount() {
-    let nameField = ReactDOM.findDOMNode(this.refs.nameField)
-    _.delay(e => {
-      nameField.focus()
-      nameField.setSelectionRange(1000, 1000);
-    }, 5)
-  }
+export default class MetricName extends Component {
+  displayName: 'MetricName'
 
   handleSubmit() {
-    this.props.handleSubmit({name: this.refs.nameField.value})
+    this.props.onChange({name: this.refs.input.value})
   }
 
   render() {
     return (
-      <form className='ui form'>
-        <TextArea
+        <TextArea className='MetricName'
             onBlur={this.handleSubmit.bind(this)}
-            id="live-input"
-            ref='nameField'
-            rows={1}
+            ref={'input'}
             tabIndex={2}
-            key={'foo'+this.props.name}
             defaultValue={this.props.name}
           >
         </TextArea>
-      </form>
     )
   }
 }
 
-export default class MetricName extends Component {
+ class MetricName3 extends Component {
   displayName: 'MetricName'
-  state = {isEditing: false}
-
-  handleClick(e) {
-    this.setState({isEditing: true})
-  }
 
   handleSubmit(e) {
-    this.setState({isEditing: false})
     this.props.onChange(e)
   }
 
   render() {
-    const {isEditing} = this.state
     const {name} = this.props || 'name'
-    return isEditing ?
-      (
-           <MetricForm
-             key={name+'foo'}
-             handleSubmit={this.handleSubmit.bind(this)}
-             name={name}
-           />
-      )
-          :
-            (
-          <span className='MetricName--plain'
-            onMouseDown={this.handleClick.bind(this)}
-          >
-            {name}
-          </span>
-            )
-
-
+    return (
+      <MetricForm
+       key={name+'foo'}
+       handleSubmit={this.handleSubmit.bind(this)}
+       name={name}
+      />
+    )
   }
 }
 
