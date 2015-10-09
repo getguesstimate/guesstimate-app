59007e2e22c2a0c8e2959c1a5761c4848b1d9213
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-10-09 12:24:20 -0700

    Delete functionality

---
+++ src/components/utility/react-dimensions.js
@@ -0,0 +1,64 @@
+import React from 'react'
+
+const style = {
+  width: '100%',
+  height: '100%',
+  padding: 0,
+  border: 0
+}
+
+function defaultGetWidth (element) {
+  return element.clientWidth
+}
+
+function defaultGetHeight (element) {
+  return element.clientHeight
+}
+
+export default function Dimensions ({ getHeight = defaultGetHeight, getWidth = defaultGetWidth } = {}) {
+  return (ComposedComponent) => {
+    return class extends React.Component {
+      // ES7 Class properties
+      // http://babeljs.io/blog/2015/06/07/react-on-es6-plus/#property-initializers
+      state = {}
+
+      // Using arrow functions and ES7 Class properties to autobind
+      // http://babeljs.io/blog/2015/06/07/react-on-es6-plus/#arrow-functions
+      updateDimensions = () => {
+        const container = React.findDOMNode(this.refs.container)
+        if (!container) {
+          throw new Error('Cannot find container div')
+        }
+        this.setState({
+          containerWidth: getWidth(container),
+          containerHeight: getHeight(container)
+        })
+      }
+
+      onResize = () => {
+        if (this.rqf) return
+        this.rqf = window.requestAnimationFrame(() => {
+          this.rqf = null
+          this.updateDimensions()
+        })
+      }
+
+      componentDidMount () {
+        this.updateDimensions()
+        window.addEventListener('resize', this.onResize, false)
+      }
+
+      componentWillUnmount () {
+        window.removeEventListener('resize', this.onResize)
+      }
+
+      render () {
+        return (
+          <div style={style} ref='container'>
+            <ComposedComponent {...this.state} {...this.props}/>
+          </div>
+        )
+      }
+    }
+  }
+}
