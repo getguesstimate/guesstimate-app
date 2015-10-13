c9bf059b426978000943df109bf9ecba3679cf93
Ozzie Gooen <ozzieagooen@gmail.com>
2015-10-13 10:18:16 -0700

    Fetch user info from guesstimate API after auth0 signin

--- src/modules/me/reducer.js
+++ src/modules/me/reducer.js
@@ -1,11 +1,23 @@
 import _ from 'lodash'
 
+let newState = null
+
 export default function me(state = {}, action) {
   switch (action.type) {
-  case 'CREATE_ME':
-    let newMe = _.cloneDeep(action.object)
-    newMe.id = _.get(newMe, 'profile.user_metadata.guesstimateId')
-    return newMe
+  case 'AUTH0_ME_LOADED':
+    return {
+      token: action.token,
+      profile: action.profile,
+      loading: true
+    }
+  case 'GUESSTIMATE_ME_LOADED':
+    newState = {
+      token: state.token,
+      id: action.id,
+      profile: action.profile,
+      loading: false
+    }
+    return newState
   case 'DESTROY_ME':
     return {}
   default:
