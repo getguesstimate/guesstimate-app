7cb82118dbdc10966d069e4972f3fd19066f8256
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-10-26 00:12:08 -0700

    Updated npm dependencies

--- src/routes/layouts/header/index.js
+++ src/routes/layouts/header/index.js
@@ -1,28 +1,7 @@
 import React, {Component, PropTypes} from 'react'
-import DropdownButton from 'react-bootstrap/lib/DropdownButton'
-import Button from 'react-bootstrap/lib/Button'
-import ButtonGroup from 'react-bootstrap/lib/ButtonGroup'
-import MenuItem from 'react-bootstrap/lib/MenuItem'
 import Icon from'react-fa'
 import Login from'../login'
 import './style.css'
-
-const NavItem = React.createClass({
-  render () {
-    return (
-    <ButtonGroup>
-        <DropdownButton title='Example Guesstimates' id='split-button-pull-right'>
-         {this.props.spaces.asMutable().map((space) => {
-              return (
-                <MenuItem href={Space.url(space)} key={space.id}>{space.name}</MenuItem>
-              )
-          })}
-        </DropdownButton>
-        <Button href="/space/new"><Icon name='plus'/></Button>
-      </ButtonGroup>
-    )
-  }
-})
 
 const Header = React.createClass({
   displayName: 'Header',
