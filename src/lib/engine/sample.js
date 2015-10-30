9956c92abc464859efecb7b505e9cd5be022920e
Ozzie Gooen <ozzieagooen@gmail.com>
2015-10-30 16:34:41 -0700

    Do not remove samples that are 0

--- src/lib/engine/sample.js
+++ src/lib/engine/sample.js
@@ -1,6 +1,6 @@
 export function combine(samples) {
   const useableSamples = samples.filter(e => e)
   const errors = [].concat.apply([], useableSamples.map(s => s.errors)).filter(s => s);
-  const values = [].concat.apply([], useableSamples.map(s => s.values)).filter(s => s);
+  const values = [].concat.apply([], useableSamples.map(s => s.values)).filter(s => _.isFinite(s));
   return {errors, values};
 }
