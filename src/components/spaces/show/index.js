3a7432f4f11e7efd4db8e06969b4d8b9bd0e5f5f
Ozzie Gooen <ozzieagooen@gmail.com>
2015-10-30 20:39:33 -0700

    Small UI changes

--- src/components/spaces/show/index.js
+++ src/components/spaces/show/index.js
@@ -39,7 +39,7 @@ export default class SpacesShow extends Component {
       <div className='hero-unit'>
         <div className='container-fluid'>
           <div className='row'>
-            <div className='col-sm-8'>
+            <div className='col-sm-10'>
 
               {space &&
                 <SpacesShowHeader onDestroy={this.destroy.bind(this)}
@@ -49,7 +49,7 @@ export default class SpacesShow extends Component {
               }
             </div>
 
-            <div className='col-sm-4'>
+            <div className='col-sm-2'>
 
               {space && space.user && !space.ownedByMe &&
                 <div>
