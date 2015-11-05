961216577e3f7ac1ef3b5de75fa952f1424b38dc
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-11-04 17:57:18 -0800

    Basic metric shown in component-index

--- src/routes/component-index.js
+++ src/routes/component-index.js
@@ -8,59 +8,56 @@ import './main.css'
 export default class ComponentEditor extends Component {
   render() {
     return (
-    <div className='row'>
-      <div className='col-sm-3'>
+    <div className='row ComponentEditor'>
+      <div className='col-sm-2'>
         <h2> {this.props.name} </h2>
-        <h4> Props </h4>
-        <JSONTree data={this.props.children.props}/>
+        <JSONTree data={this.props.childProps}/>
       </div>
-      <div className='col-sm-9'>
-        {this.props.children}
+      <div className='col-sm-10 Component'>
+        {<this.props.child {...this.props.childProps}/>}
       </div>
     </div>
     )
   }
 }
 
+const MetricCardProps = {
+  metric: {
+    id: '123',
+    space: 32,
+    readableId: 'AB',
+    name: 'Population of Chicago',
+    location: {row: 3, column: 9},
+    guesstimate: {
+      metric: '123',
+      input: '3->8'
+    },
+    simulation: {
+      metric: '123',
+      sample: {
+        errors: [],
+        values: [30.3, 30.9, 31.9, 39.4, 50.3, 80.2]
+      },
+      stats: {
+        mean: 40,
+        stdev: 5,
+        length: 6
+      },
+    }
+  },
+  canvasState: 'scientific',
+  location: {row: 3, column: 3},
+  userAction: 'selecting',
+  handleSelect: () => {console.log('select')},
+  isSelected: true
+}
+
 export default class ComponentIndex extends Component{
   displayName: 'ComponentIndex'
   render () {
-    const metric = {
-      id: '123',
-      space: 32,
-      readableId: 'AB',
-      name: 'Population of Chicago',
-      location: {row: 3, column: 9},
-      guesstimate: {
-        metric: '123',
-        input: '3->8'
-      },
-      simulation: {
-        metric: '123',
-        sample: {
-          errors: [],
-          values: [30.3, 30.9, 31.9, 39.4, 50.3, 80.2]
-        },
-        stats: {
-          mean: 40,
-          stdev: 5,
-          length: 6
-        },
-      }
-    }
     return (
-      <div className='container'>
-      <ComponentEditor name='MetricCard'>
-      <MetricCard
-          canvasState={'scientific'}
-          handleSelect={() => {console.log('select')}}
-          key={12}
-          location={{row: 1, column:3}}
-          metric={metric}
-          userAction={'selecting'}
-          isSelected={true}
-      />
-</ComponentEditor>
+      <div className='container-fluid full-width'>
+      <ComponentEditor name='MetricCard' child={MetricCard} childProps={MetricCardProps}/>
       </div>
     )
   }
