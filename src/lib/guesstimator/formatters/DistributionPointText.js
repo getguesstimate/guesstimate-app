237feae5f95e92c5747f9dae39a08185a56dfeff
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-11-11 12:57:46 -0800

    Added guesstimator formatters, point formatter working

---
+++ src/lib/guesstimator/formatters/DistributionPointText.js
@@ -0,0 +1,16 @@
+import {textMixin, isParseableNumber, parseNumber} from './lib.js'
+
+export const item = Object.assign(
+  textMixin,
+  {
+    guesstimateType: 'POINT',
+    inputType: 'TEXT',
+    errors(g) { return [] },
+    format(g) {
+      const {guesstimateType} = this
+      const value = parseNumber(g.text)
+      return {guesstimateType, value}
+    },
+    _matchesText(text) { return isParseableNumber(text) },
+  }
+)
