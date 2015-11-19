10451da80dfe7fc2a06818e32c7bad1cf8af5dd0
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-11-18 16:50:09 -0800

    Normal Distribution should handle negative numbers

--- src/lib/guesstimator/samplers/DistributionNormal.js
+++ src/lib/guesstimator/samplers/DistributionNormal.js
@@ -5,10 +5,14 @@ export var Sampler = {
   sample(formatted, n) {
     const mean = (formatted.high - formatted.low)/2 + formatted.low
     const stdev = formatted.high - mean
-    let inputs = {mean, stdev, seed: 'SEED'}
+    let inputs = {mean, stdev, min: Infinity}
 
     if (_.isFinite(formatted.minimum)){ inputs.min = formatted.minimum }
     if (_.isFinite(formatted.maximum)){ inputs.max = formatted.maximum }
+
+    if (inputs.mean === 0) {
+      inputs.mean = Math.pow(10, -100)
+    }
 
     const stochator = new Stochator(inputs);
     let results = stochator.next(n)
