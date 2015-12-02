48973334800e277e6703660186ec0e92e1add509
Ozzie Gooen <ozzieagooen@gmail.com>
2015-12-02 12:06:29 -0800

    Refactored click-to-edit to allow clickable links in document

--- src/components/utility/click-to-edit/index.js
+++ src/components/utility/click-to-edit/index.js
@@ -4,6 +4,55 @@ import Icon from 'react-fa'
 import './style.css'
 import {ButtonClose} from '../buttons/close/index.js'
 
+export default class EditingMode extends Component {
+  displayName: 'ClickToEditEditing'
+
+  _submit() {
+    const value = this.textInput.value
+    this.props.onSubmit(value)
+    this.props.onClose()
+  }
+
+  render() {
+    return(
+      <div className='EditingMode'>
+        <textarea  defaultValue={this.props.value} ref='input' ref={ (ref) =>
+          {
+            ref && React.findDOMNode(ref).select();
+            this.textInput = ref
+          }
+        } />
+        <div className='submit-section'>
+          <div className='submit ui button primary' onClick={this._submit.bind(this)}>
+            {this.props.editingSaveText}
+          </div>
+          <ButtonClose onClick={this.props.onClose}/>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default class ViewingMode extends Component {
+  displayName: 'ClickToEditViewing'
+
+  render() {
+    return(
+      <div className='ViewingMode'>
+        <div className='row'>
+          <div className='col-sm-12 header'>
+            Description
+            <span className='editLink' onClick={this.props.onEdit}>Edit </span>
+          </div>
+          <div className='col-sm-12 content'>
+            {this.props.children}
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 export default class ClickToEdit extends Component {
   displayName: 'ClickToEdit'
 
@@ -24,39 +73,32 @@ export default class ClickToEdit extends Component {
     this.setState({isEditing: false})
   }
 
-  _toggle() {
-    this.state.isOpen ? this._close() : this._open()
-  }
-
-  _submit() {
-    const value = this.textInput.value
-    this.props.onSubmit(value)
-    this._close()
-  }
-
   render() {
-    let className = 'click-to-edit'
-    className += this.state.isEditing ? ' editing' : ' viewing'
+    const showEditing = this.state.isEditing
+    const showDescription = !showEditing && !_.isEmpty(this.props.value)
+    const showAddDescription = !showEditing && !showDescription
+
     return (
-      <span className={className} onClick={!this.state.isEditing && this._open.bind(this)}>
-        {this.state.isEditing &&
-          <textarea  defaultValue={this.props.value} ref='input' ref={ (ref) =>
-            {
-              ref && React.findDOMNode(ref).select();
-              this.textInput = ref
-            }
-          } />
+      <span className='ClickToEdit'>
+        {showEditing &&
+          <EditingMode
+            value={this.props.value}
+            editingSaveText={this.props.editingSaveText}
+            onSubmit={this.props.onSubmit}
+            onClose={this._close.bind(this)}
+            />
         }
-        {this.state.isEditing &&
-          <div className='submit-section'>
-            <div className='submit ui button primary' onClick={this._submit.bind(this)}>
-              {this.props.editingSaveText}
-            </div>
-            <ButtonClose onClick={this._close.bind(this)}/>
+
+        {showAddDescription &&
+          <div className='BlankMode' onClick={this._open.bind(this)}>
+            {this.props.emptyValue}
           </div>
         }
-        {!this.state.isEditing &&
-          (_.isEmpty(this.props.value) ? this.props.emptyValue : this.props.viewing)
+
+        {showDescription &&
+          <ViewingMode onEdit={this._open.bind(this)}>
+            {this.props.viewing}
+          </ViewingMode>
         }
       </span>
     )
