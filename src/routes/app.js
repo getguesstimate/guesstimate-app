286b6a2c6a492275ad9a239554f08e60b44bdff1
Ozzie Gooen <Ozzie@grandroundshealth.com>
2015-12-03 16:25:54 -0800

    Added basic Segment.io page tracking

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,10 +1,12 @@
 import app from 'ampersand-app'
 import Router from './router'
+import * as segment from '../server/segment/index.js'
 
 window.app = app
 
 app.extend({
   init () {
+    segment.initialize()
     this.router = new Router()
     this.router.history.start()
   }
---
+++ src/server/segment/constants.js
@@ -0,0 +1,1 @@
+export const API_KEY = 'XIOVIJJihFrKFeGH6KwxT3ve2Y1Bxnta'
