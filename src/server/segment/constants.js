4c4ba3a1e0fec56d553729fa2991f8b6efa39c0a
Ozzie Gooen <ozzieagooen@gmail.com>
2015-12-03 16:48:08 -0800

    Added production segment.io creds

--- src/server/segment/constants.js
+++ src/server/segment/constants.js
@@ -1,1 +1,3 @@
-export const API_KEY = 'XIOVIJJihFrKFeGH6KwxT3ve2Y1Bxnta'
+const DEV_API_KEY = 'XIOVIJJihFrKFeGH6KwxT3ve2Y1Bxnta'
+const PRODUCTION_API_KEY = 'tFCMysKzusXbVgq593tbROJQYEt57Eei'
+export const API_KEY = (__API_ENV__ === 'development') ? DEV_API_KEY : PRODUCTION_API_KEY
