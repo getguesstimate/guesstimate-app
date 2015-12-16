4f835aea699f4d51eac3cd5f07c0e51e62a8f3c1
Ozzie Gooen <ozzieagooen@gmail.com>
2015-12-16 14:33:33 -0800

    Added intercom basic version

--- src/routes/app.js
+++ src/routes/app.js
@@ -3,10 +3,12 @@ import Router from './router'
 import * as segment from '../server/segment/index.js'
 import * as sentry from '../server/sentry/index.js'
 
-window.app = app
-
 app.extend({
   init () {
+    window.intercomSettings = {
+      app_id: "o0trb1v9"
+    };
+    (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/o0trb1v9';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
     segment.initialize()
     sentry.initialize()
     this.router = new Router()
