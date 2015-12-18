9581ecb8a5ba2299deb312a3e9b949a56217b399
Ozzie Gooen <ozzieagooen@gmail.com>
2015-12-18 15:57:31 -0800

    Added formatter so that range can be added in brackets

---
+++ src/lib/guesstimator/formatter/formatters/DistributionTextUpToAlternate.js
@@ -0,0 +1,18 @@
+import {normalTextMixin} from './lib.js'
+
+export const item = Object.assign(
+  {}, normalTextMixin,
+  {
+    inputType: 'TEXT',
+    formatterName: 'DISTRIBUTION_NORMAL_TEXT_UPTO',
+    _symbols: ['['],
+    format(g) {
+      const guesstimateType = this.guesstimateType(g)
+      const [low, high] = this._numbers(g.text)
+      return {guesstimateType, low, high }
+    },
+    _numbers(text) {
+      return this._splitNumbersAt(text.replace('[', '').replace(']', ''), ',')
+    },
+  }
+)
---
+++ src/lib/guesstimator/formatter/formatters/tests/DistributionTextUpToAlternate-test.js
@@ -0,0 +1,32 @@
+import {item as formatter} from '../DistributionTextUpToAlternate.js'
+
+describe('DistributionTextUpTo', () => {
+  describe('#matches', () => {
+    const examples = [
+      [{text: '[8,10]', guesstimateType: 'UNIFORM'}, true],
+      [{text: '[8,10', guesstimateType: 'UNIFORM'}, true],
+      [{text: '8,10]', guesstimateType: 'UNIFORM'}, false],
+    ]
+
+    examples.map(e => () => {
+      it(`works for guesstimate ${JSON.stringify(e[0])}`, () => {
+        expect(formatter.matches(e[0])).to.equal(e[1])
+      })
+    }).map(e => e())
+  });
+
+  describe('#format', () => {
+    const examples = [
+      [{text: '[8, 10]'}, {guesstimateType: 'NORMAL', low: 8, high: 10}],
+      [{text: '[8, 10]', guesstimateType: 'UNIFORM'}, {guesstimateType: 'UNIFORM', low: 8, high: 10}], [{text: '[8,10]', guesstimateType: 'NORMAL'}, {guesstimateType: 'NORMAL', low: 8, high: 10}],
+      [{text: '[-5, 5]', guesstimateType: 'NORMAL'}, {guesstimateType: 'NORMAL', low: -5, high: 5}],
+      [{text: '[0, 5]', guesstimateType: 'NORMAL'}, {guesstimateType: 'NORMAL', low: 0, high: 5}],
+    ]
+
+    examples.map(e => () => {
+      it(`guesstimate ${JSON.stringify(e[0])} converts to ${JSON.stringify(e[1])}`, () => {
+        expect(formatter.format(e[0])).to.deep.equal(e[1])
+      })
+    }).map(e => e())
+  });
+})
