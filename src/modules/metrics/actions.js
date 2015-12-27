209245c2bd353be346d8e7da438f3ec2dad644b6
Ozzie Gooen <ozzieagooen@gmail.com>
2015-12-26 21:41:15 -0800

    Autosave hook created for metric changes

--- src/modules/metrics/actions.js
+++ src/modules/metrics/actions.js
@@ -1,17 +1,39 @@
 import e from 'gEngine/engine';
+import * as spaceActions from 'gModules/spaces/actions.js'
+
+function findSpaceId(getState, metricId) {
+  const metric = e.metric.get(getState().metrics, metricId)
+  console.log('here1', metric)
+  return _.get(metric, 'space')
+}
+
+function registerChange(dispatch, spaceId) {
+  spaceId && dispatch(spaceActions.registerChange(spaceId));
+}
 
 export function addMetric(item) {
   return (dispatch, getState) => {
     const existingReadableIds = getState().metrics.map(m => m.readableId)
     let newItem = Object.assign(item, e.metric.create(existingReadableIds))
+
     dispatch({ type: 'ADD_METRIC', item: newItem });
+    registerChange(dispatch, findSpaceId(getState, item.id))
   }
 }
 
+//spaceId must be done before the metric is removed here.
 export function removeMetric(id) {
-  return { type: 'REMOVE_METRIC', item: {id: id}};
+  return (dispatch, getState) => {
+    const spaceId = findSpaceId(getState, id)
+
+    dispatch({ type: 'REMOVE_METRIC', item: {id: id}});
+    registerChange(dispatch, spaceId)
+  }
 }
 
 export function changeMetric(item) {
-  return { type: 'CHANGE_METRIC', item };
+  return (dispatch, getState) => {
+    dispatch({ type: 'CHANGE_METRIC', item });
+    registerChange(dispatch, findSpaceId(getState, item.id))
+  }
 }
