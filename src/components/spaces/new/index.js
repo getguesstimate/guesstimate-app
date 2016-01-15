3fe8c82484636f1418bc653dd4bb2aa2f9a0b4f8
Ozzie Gooen <Ozzie@grandroundshealth.com>
2016-01-15 11:34:21 -0800

    show has_private_access users private option

--- src/components/spaces/new/index.js
+++ src/components/spaces/new/index.js
@@ -10,28 +10,45 @@ function mapStateToProps(state) {
   }
 }
 
-@connect()
+@connect(mapStateToProps)
 export default class NewSpaceFormContainer extends Component {
   onSubmit(e) {
     e.preventDefault()
-    const params = serialize(this.refs.form, {hash: true})
+    let params = serialize(this.refs.form, {hash: true})
+
+    if (this.canUsePrivateModels()) {
+      params['is_private'] = (params.is_public === 'on') ? false : true
+    }
+
     this.props.dispatch(spaceActions.create(params))
   }
 
+  canUsePrivateModels() {
+    return !!_.get(this.props,  'me.profile.has_private_access')
+  }
+
   render() {
+    const canUsePrivateModels = this.canUsePrivateModels()
     return (
       <div className='SpaceNew' >
         <div className='row'>
           <div className='col-md-2'>
           </div>
           <div className='col-md-8'>
-            <h2> Create a Public Model </h2>
+            <h2> Create a {canUsePrivateModels ? '' : 'Public'} Model </h2>
             <br/>
             <form onSubmit={this.onSubmit.bind(this)} className='ui form' ref='form'>
                 <div className='field'>
                   <label>Name</label>
                   <input type="text" name="name"/>
                 </div>
+
+                {canUsePrivateModels &&
+                  <div className='field'>
+                    <label>Public?</label>
+                    <input type="checkbox" name="is_public"/>
+                  </div>
+                }
 
                 <button type='submit' className='ui button primary'>
                   {'Create'}
