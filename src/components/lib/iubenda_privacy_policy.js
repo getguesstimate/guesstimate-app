60489d3037cf73c459f888093d921b9ae4b4467a
Ozzie Gooen <Ozzie@grandroundshealth.com>
2016-02-05 12:27:01 -0800

    Added privacy policy page

---
+++ src/components/lib/iubenda_privacy_policy.js
@@ -0,0 +1,34 @@
+import React, {Component, PropTypes} from 'react'
+
+export default class IubendaPrivacyPolicy extends Component{
+  displayName: 'PrivacyPolicy'
+
+  static propTypes = {
+    id: React.PropTypes.number
+  }
+
+  componentWillMount () {
+    var loadIubenda = function (w,d) {
+      var loader = function () {
+        var s = d.createElement("script")
+        var tag = d.getElementsByTagName("script")[0]
+        s.src = "//cdn.iubenda.com/iubenda.js"
+        tag.parentNode.insertBefore(s,tag)
+      }
+      if(w.addEventListener){
+        w.addEventListener("load", loader, false)
+      } else if(w.attachEvent){
+        w.attachEvent("onload", loader)
+      } else {
+        w.onload = loader;
+      }}
+    loadIubenda(window, document)
+  }
+
+  render() {
+    const href = "//www.iubenda.com/privacy-policy/" + this.props.id
+    return(
+      <a href={href} className="iubenda-nostyle no-brand iubenda-embed" title="Privacy Policy">{this.props.children}</a>
+    )
+  }
+}
---
+++ src/components/pages/privacy_policy/index.js
@@ -0,0 +1,15 @@
+import React, {Component, PropTypes} from 'react'
+import IubendaPrivacyPolicy from 'gComponents/lib/iubenda_privacy_policy.js'
+
+export default class PrivacyPolicy extends Component{
+  displayName: 'PrivacyPolicy'
+  render () {
+    return (
+      <div>
+        <IubendaPrivacyPolicy id={7790420}>
+          Privacy Policy
+        </IubendaPrivacyPolicy>
+      </div>
+    )
+  }
+}
