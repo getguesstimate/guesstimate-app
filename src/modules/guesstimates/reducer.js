82baf5166f75f7e9ea7d22b75fd0d7974cf65fcd
Ozzie Gooen <Ozzie@grandroundshealth.com>
2016-02-11 21:57:05 -0800

    Ensure than metrics and guesstimates are unique

--- src/modules/guesstimates/reducer.js
+++ src/modules/guesstimates/reducer.js
@@ -1,23 +1,24 @@
 import engine from 'gEngine/engine'
 
+function uniq(items) {
+  return _.uniqBy(items, 'metric')
+}
+
 export default function guesstimates(state = [], action) {
   switch (action.type) {
   case 'SPACES_FETCH_SUCCESS':
     let newGuesstimates = _.flatten(action.records.map(e => _.get(e, 'graph.guesstimates'))).filter(e => e)
-    return [...state, ...newGuesstimates]
+    return uniq([...state, ...newGuesstimates])
   case 'ADD_METRIC':
-    return [...state, {metric: action.item.id, input: '', guesstimateType: 'NONE', description: ''}]
+    return uniq([...state, {metric: action.item.id, input: '', guesstimateType: 'NONE', description: ''}])
   case 'REMOVE_METRIC':
     return state.filter(y => y.metric !== action.item.id)
   case 'CHANGE_GUESSTIMATE':
     const i = state.findIndex(y => y.metric === action.values.metric);
+    const newItem = engine.guesstimate.format(action.values)
     if (i !== -1) {
-      const newState = [
-        ...state.slice(0, i),
-        engine.guesstimate.format(action.values),
-        ...state.slice(i+1, state.length)
-      ];
-      return newState
+      const newState = [ ...state.slice(0, i), newItem, ...state.slice(i+1, state.length) ];
+      return uniq(newState)
     }
   default:
     return state;
