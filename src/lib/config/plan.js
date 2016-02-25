381a1024fca8eebef521d27abedb5e5876d581a1
Ozzie Gooen <ozzieagooen@gmail.com>
2016-02-24 18:22:15 -0800

    Added plan information as yaml

---
+++ src/lib/config/plan.js
@@ -0,0 +1,27 @@
+import planYaml from 'json!yaml!./plans.yml'
+
+export default class Plan {
+  static asHashes() {
+    return planYaml.plans
+  }
+
+  static all() {
+    const that = this
+    return this.asHashes().map(e => new that(e))
+  }
+
+  static find(id) {
+    return this.all().filter(e => (e.id === id))[0]
+  }
+
+  constructor({id, monthly_cost, name, private_model_limit}) {
+    this.id = id;
+    this.monthlyCost = monthly_cost;
+    this.name = name;
+    this.privateModelLimit = private_model_limit
+  }
+
+  fullName() {
+    return `${this.name} Plan`
+  }
+}
