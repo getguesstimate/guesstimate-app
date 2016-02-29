0b1102b1717f45967d762673605406eea4233047
Ozzie Gooen <ozzieagooen@gmail.com>
2016-02-28 20:31:32 -0800

    Open modal for pricing

--- src/components/plans/index/container.js
+++ src/components/plans/index/container.js
@@ -1,6 +1,7 @@
 import React, {Component, PropTypes} from 'react'
 import { connect } from 'react-redux';
 import PlanIndex from './PlanIndex.js'
+import * as modalActions from 'gModules/modal/actions.js'
 
 function mapStateToProps(state) {
   return {
@@ -12,11 +13,15 @@ function mapStateToProps(state) {
 export default class PlanIndexContainer extends Component{
   displayName: 'PlanIndexContainer'
 
+  _onChoose(planId) {
+    this.props.dispatch(modalActions.openFirstSubscription(planId))
+  }
+
   render () {
     const {me} = this.props
     const portalUrl = _.get(me, 'profile.account._links.payment_portal.href')
     const userPlanId = _.get(me, 'profile.plan.id')
-    const props = {portalUrl, userPlanId}
+    const props = {portalUrl, userPlanId, onChoose: this._onChoose.bind(this)}
 
     return (
       <PlanIndex {...props}/>
