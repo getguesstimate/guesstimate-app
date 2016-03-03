69c09dea7c1e48bafc530591114b080724c77572
Ozzie Gooen <ozzieagooen@gmail.com>
2016-03-03 01:40:01 -0800

    Removing uneccessary code

--- src/lib/guesstimator/formatter/formatters/Function.js
+++ src/lib/guesstimator/formatter/formatters/Function.js
@@ -16,19 +16,6 @@ export const item = Object.assign(
     },
 
     errors(g) {return []},
-    inputMetrics(g, dGraph) { return inputMetrics(g.text, dGraph) },
     _formatText(text) { return text.substring(1, text.length) },
   }
 )
-
-//export function metricGraphToInputs(text, dGraph) {
-  //const metrics = inputMetrics(text, dGraph)
-  //let inputs = {}
-  //metrics.map(m => {inputs[m.readableId] = _.get(m, 'simulation.sample.values') })
-  //return inputs
-//}
-
-//export function inputMetrics(text, dGraph) {
-  //if (!_.has(dGraph, 'metrics')){ return [] }
-  //return dGraph.metrics.filter((m) => { return text.includes(m.readableId); });
-//}
--- src/lib/guesstimator/index.js
+++ src/lib/guesstimator/index.js
@@ -1,5 +1,1 @@
-import {format, errors, preFormatGuesstimate} from './formatter/index.js'
-import {find} from './types.js'
-
-export {inputMetrics} from './formatter/index.js'
 export {Guesstimator} from './guesstimator.js'
