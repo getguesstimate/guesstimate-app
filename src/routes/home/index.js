ae5396c67aa6e7438b7c487fa153d6ec2e5d37b7
Ozzie Gooen <ozzieagooen@gmail.com>
2016-03-07 23:31:29 -0800

    First pass at homepage benefits section

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -8,38 +8,68 @@ export default class Home extends Component {
   displayName: 'Home'
   render () {
     return (
-      <div className='container-fluid full-width homePage'>
-        <div className='row'>
-          <div className='col-md-2'></div>
-          <div className='col-md-8 main'>
-            <div className='row'>
-              <div className='col-sm-12'>
-                <div className='guesstimate-logo-outer'>
-                  <div className='guesstimate-logo'>
-                    <img src={LogoWord} />
+      <div className='homePage'>
+        <div className='container-fluid full-width'>
+          <div className='row'>
+            <div className='col-md-2'></div>
+            <div className='col-md-8 main'>
+              <div className='row'>
+                <div className='col-sm-12'>
+                  <div className='guesstimate-logo-outer'>
+                    <div className='guesstimate-logo'>
+                      <img src={LogoWord} />
+                    </div>
                   </div>
                 </div>
-              </div>
 
-              <div className='col-sm-12 guesstimate-slogan'>
-                <h2>A spreadsheet for things<br/>that aren&rsquo;t certain</h2>
-              </div>
+                <div className='col-sm-12 guesstimate-slogan'>
+                  <h2>A spreadsheet for things<br/>that aren&rsquo;t certain</h2>
+                </div>
 
+              </div>
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-xs-12 cta'>
+              <a href='/models' className='ui button huge primary'> Browse Public Models </a>
+            </div>
+          </div>
+
+          <div className='row'>
+            <div className='col-xs-12'>
+              <span className="wistia_embed wistia_async_ua8kd9n06a popover=true popoverAnimateThumbnail=true videoFoam=true" >
+                &nbsp;
+              </span>
             </div>
           </div>
         </div>
-
-        <div className='row'>
-          <div className='col-xs-12 cta'>
-            <a href='/models' className='ui button huge primary'> Browse Public Models </a>
-          </div>
-        </div>
-
-        <div className='row'>
-          <div className='col-xs-12'>
-            <span className="wistia_embed wistia_async_ua8kd9n06a popover=true popoverAnimateThumbnail=true videoFoam=true" >
-              &nbsp;
-            </span>
+        <div className='container-fluid full-width sectionBenefits'>
+          <div className='wrap'>
+            <div className='row'>
+              <div className='col-sm-4'>
+                <h2> Simple </h2>
+                <p>
+                  Guesstimate takes minutes to learn. <br/>
+                  If you think a number is between 5 and 9, <br/>
+                  express that by writing [5,9]
+                </p>
+              </div>
+              <div className='col-sm-4'>
+                <h2> Powerful </h2>
+                <p>
+                  Guesstimate uses Monte Carlo modeling techniques to simulate functions performed on ranges.
+                </p>
+              </div>
+              <div className='col-sm-4'>
+                <h2> Free </h2>
+                <p>
+                  Create unlimitted public models. <br/>
+                  Brows from others internationally.<br/>
+                  Also, it's open source.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
