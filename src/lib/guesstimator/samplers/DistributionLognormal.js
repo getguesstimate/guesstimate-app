b1417b5994f194ae61fe5bb6d453a840c9ced737
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-09 19:27:17 -0800

    Added lognormal symmetric CI based distribution.

--- src/lib/guesstimator/samplers/DistributionLognormal.js
+++ src/lib/guesstimator/samplers/DistributionLognormal.js
@@ -1,10 +1,12 @@
+import math from 'mathjs';
 var jStat = require('jstat').jStat;
 
 export var Sampler = {
   sample(formatted, n) {
-    // This assumes a 90% confidence interval
-    logHigh = math.log(formatted.high)
-    logLow = math.log(formatted.low)
+    // This assumes a centered 90% confidence interval, e.g. the left endpoint
+    // marks 0.05% on the CDF, the right 0.95%.
+    const logHigh = math.log(formatted.high)
+    const logLow = math.log(formatted.low)
 
     const mean = (logHigh + logLow)/2
     const stdev = (logHigh-logLow) / (2*1.645)
