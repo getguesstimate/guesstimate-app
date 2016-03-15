680f29b07865ab1d64bc5f89882fafe2c51b3fbb
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-15 15:37:09 -0700

    Added segment tracking functions for purchase success and failure.

--- src/server/segment/constants.js
+++ src/server/segment/constants.js
@@ -1,3 +1,8 @@
+// API Access Constants
 const DEV_API_KEY = 'XIOVIJJihFrKFeGH6KwxT3ve2Y1Bxnta'
 const PRODUCTION_API_KEY = 'HXZO1nlg9qD4tfCcSSqdcjxq3pGaF0eU'
 export const API_KEY = (__API_ENV__ === 'development') ? DEV_API_KEY : PRODUCTION_API_KEY
+
+// Event Tracking Constants
+export const PURCHASE_SUCCESS_ACTION = "Successful Purchase"
+export const PURCHASE_FAILURE_ACTION = "Failed Purchase"
