be4fad11e8cfd482d063e971a9fd37eae4b9a625
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-15 18:54:49 -0700

    Tracking account modal clicks and user menu opens/closes

--- src/server/segment/index.js
+++ src/server/segment/index.js
@@ -1,4 +1,4 @@
-import {API_KEY, PURCHASE_SUCCESS_ACTION} from './constants.js'
+import {API_KEY, PURCHASE_SUCCESS_ACTION, ACCOUNT_MODAL_CLICK, USER_MENU_OPEN, USER_MENU_CLOSE} from './constants.js'
 
 export function initialize() {
     !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
@@ -16,4 +16,16 @@ export function trackUser(userId, info) {
 
 export function trackPurchaseSuccess(planType) {
   window.analytics.track(PURCHASE_SUCCESS_ACTION, {plan: planType})
+}
+
+export function trackAccountModalClick() {
+  window.analytics.track(ACCOUNT_MODAL_CLICK)
+}
+
+export function trackUserMenuOpen() {
+  window.analytics.track(USER_MENU_OPEN)
+}
+
+export function trackUserMenuClose() {
+  window.analytics.track(USER_MENU_CLOSE)
 }
