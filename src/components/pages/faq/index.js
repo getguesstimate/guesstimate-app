e8964d571a3e9cf32f6dda5737f618bcc45f34f6
Ozzie Gooen <ozzieagooen@gmail.com>
2016-03-15 18:40:39 -0700

    Added footer link and updated faq

--- src/components/pages/faq/index.js
+++ src/components/pages/faq/index.js
@@ -11,13 +11,13 @@ Guesstimate is great at making estimates of things that aren't certain.  You can
 The most popular model on Guesstimate is [How Long it Takes to Get Ready for Preschool](/models/314).  We also recommend checking out Ozzie Gooen's models [here](/users/1).
 
 ## Are there private models?
-Not yet.  Please sign up for an account to get on our mailing list, or follow us [on Twitter](https://twitter.com/getguesstimate) to be notified once there are.  We expect private models in the next 1-6 weeks.
+Yes. Check out our [pricing page](/pricing) for more information.
 
 ## What functions are available on Guesstimate?
 You can find a list by clicking on the **Documentation** widget on the right, then clicking **Functions: Available Existing Functions**.
 
 ## Can I edit someone else's model?
-Currently all models on Guesstimate are public.  Your own models autosave when you edit them.  If you edit models created by other people, you will be able to modify them, but your changes will not be saved.  This can be great for experimentation or for entering your own assumptions in a different model.
+Your own models autosave when you edit them.  If you edit models created by other people, you will be able to modify them, but your changes will not be saved.  This can be great for experimentation or for entering your own assumptions in a different model.
 
 ## How is this different from Oracle Crystal Ball or other existing tools?
 Guesstimate is a web application, while Crystal Ball is a suite of Excel-based applications. This means that with Guesstimate you can get up and running in 30 seconds on any computer and then share your model with anyone. Crystal Ball is better for more rigorous analysis, and gives you access to the Excel environment and ecosystem.
@@ -31,7 +31,7 @@ Due to the great amount of interest, there are plans to make Guesstimate into a 
 Ozzie Gooen.  You can find him [on Twitter](https://twitter.com/ozziegooen).
 
 ## Is it open source?
-Currently the client code is open source.  You can check out the [GitHub repo](https://github.com/getguesstimate/guesstimate-app).  There is a small server component that is not available.
+Yes. You can check out the [GitHub repo](https://github.com/getguesstimate/guesstimate-app).
 
 ## Where can I find more information on Guesstimate?
 We have a blog [here](https://medium.com/guesstimate-blog).
@@ -43,7 +43,7 @@ You can use normal, lognormal, and uniform distributions from the standard input
 You can find a list by clicking on the **Documentation** widget on the right, then clicking **Functions: Additional Distributions**. There are also a few [discrete distributions](http://mathjs.org/docs/reference/functions/categorical.html) using the math.js library.
 
 ## How many samples are done per metric?
-Approximately 5000.
+5000.
 
 ## Why are there 5000 samples done per metric?
 5000 is enough to be useful for most estimates, but not enough to slow the system down.  In the future, this amount may be variable depending on the need and circumstances.
