885ca663d12b98e9e9034c6912ab094239c96ff8
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-16 20:30:24 -0700

    Making lognormal quto-inferred when bounds are positive

--- src/lib/guesstimator/formatter/formatters/DistributionTextUpToAlternate.js
+++ src/lib/guesstimator/formatter/formatters/DistributionTextUpToAlternate.js
@@ -7,9 +7,12 @@ export const item = Object.assign(
     formatterName: 'DISTRIBUTION_NORMAL_TEXT_UPTO',
     _symbols: ['['],
     format(g) {
-      const guesstimateType = this.guesstimateType(g)
-      const [low, high] = this._numbers(g.text)
-      return {guesstimateType, low, high }
+      let [low, high] = this._numbers(g.text)
+      if (!(isNaN(low) || isNaN(high))) {
+        const guesstimateType = this.guesstimateType(g, low > 0 ? 'LOGNORMAL' : 'NORMAL')
+        return {guesstimateType, low, high }
+      }
+      return {}
     },
     _numbers(text) {
       return this._splitNumbersAt(text.replace('[', '').replace(']', ''), ',')
