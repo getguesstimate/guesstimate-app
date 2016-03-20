7bc4764d7b3662d93d8a6f54d405ced73885add5
Matthew McDermott <mattmcdermott8@gmail.com>
2016-03-20 15:18:49 -0700

    Basic Commerce Tracking (renaming event name to specialty commerce event name).

--- src/server/segment/constants.js
+++ src/server/segment/constants.js
@@ -4,7 +4,7 @@ const PRODUCTION_API_KEY = 'HXZO1nlg9qD4tfCcSSqdcjxq3pGaF0eU'
 export const API_KEY = (__API_ENV__ === 'development') ? DEV_API_KEY : PRODUCTION_API_KEY
 
 // Event Tracking Constants
-export const PURCHASE_SUCCESS_ACTION = "Successfully purchased"
+export const PURCHASE_SUCCESS_ACTION = "Completed Order"
 export const ACCOUNT_MODAL_CLICK = "Clicked account modal"
 export const USER_MENU_OPEN = "Opened user menu"
 export const USER_MENU_CLOSE = "Closed user menu"
