4fd4631d21973dbe1fc7d5bac09632d38efcd052
Ozzie Gooen <Ozzie@grandroundshealth.com>
2016-04-05 01:12:49 -0700

    Edges relate to guesstimateForm

--- src/components/spaces/denormalized-space-selector.js
+++ src/components/spaces/denormalized-space-selector.js
@@ -4,16 +4,20 @@ import e from 'gEngine/engine'
 const spaceGraphSelector = state => { return _.pick(state, 'spaces', 'metrics', 'guesstimates', 'simulations', 'users', 'organizations', 'userOrganizationMemberships', 'me') }
 const spaceSelector = (state, props) => {return state.spaces.find(s => s.id.toString() === props.spaceId.toString())}
 const canvasStateSelector = state => {return state.canvasState}
+const guesstimateFormSelector = state => {return state.guesstimateForm}
 
 export const denormalizedSpaceSelector = createSelector(
   spaceGraphSelector,
   spaceSelector,
   canvasStateSelector,
-  (graph, space, canvasState) => {
+  guesstimateFormSelector,
+  (graph, space, canvasState, guesstimateForm) => {
     let dSpace = space && Object.assign(space.asMutable(), e.space.toDgraph(space.id, graph))
 
     if (dSpace) {
-      dSpace.edges = e.dgraph.dependencyMap(dSpace)
+      let bGraph = e.dgraph.toBizarroGraph(dSpace, guesstimateForm)
+      dSpace.edges = e.dgraph.dependencyMap(bGraph)
+
       dSpace.canvasState = canvasState
       dSpace.metrics = dSpace.metrics.map(s => {
         let edges = {}
