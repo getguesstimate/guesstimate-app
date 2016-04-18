df5397b82a05a347ba11a54902e97c830f668873
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-18 09:47:23 -0700

    Removing uniform prior as makes small numbers unintuitive

--- src/lib/guesstimator/samplers/DistributionBeta.js
+++ src/lib/guesstimator/samplers/DistributionBeta.js
@@ -4,8 +4,8 @@ import {jStat} from 'jstat'
 
 export var Sampler = {
   sample({hits, total}, n) {
-    // This assumes a uniform prior on likelihood.
-    return { values: Sample(n, () => jStat.beta.sample(hits+1, total-hits+1)) }
+    // This treats your entry as a prior.
+    return { values: Sample(n, () => jStat.beta.sample(hits, total-hits)) }
   }
 }
 
