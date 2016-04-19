d143068b71a3e1bb10eeb8787c009df8c6ae6662
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-19 12:36:29 -0700

    2 is better than 3

--- src/lib/guesstimator/samplers/DistributionBeta.js
+++ src/lib/guesstimator/samplers/DistributionBeta.js
@@ -4,9 +4,9 @@ import {jStat} from 'jstat'
 
 export var Sampler = {
   sample({hits, total}, n) {
-    // This treats your entry as a prior, and assumes you are 3 times more confident than
+    // This treats your entry as a prior, and assumes you are 2 times more confident than
     // a raw beta would be. This gives your distribution more of a peak for small numbers.
-    return { values: Sample(n, () => jStat.beta.sample(3*hits, 3*(total-hits))) }
+    return { values: Sample(n, () => jStat.beta.sample(2*hits, 2*(total-hits))) }
   }
 }
 
