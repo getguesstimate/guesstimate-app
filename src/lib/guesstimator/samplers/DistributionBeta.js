5b1108ea8b3ab526969fdbe76d8ab462a7361dca
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-19 12:23:06 -0700

    Increasing confidence for beta entry.

--- src/lib/guesstimator/samplers/DistributionBeta.js
+++ src/lib/guesstimator/samplers/DistributionBeta.js
@@ -4,8 +4,9 @@ import {jStat} from 'jstat'
 
 export var Sampler = {
   sample({hits, total}, n) {
-    // This treats your entry as a prior.
-    return { values: Sample(n, () => jStat.beta.sample(hits, total-hits)) }
+    // This treats your entry as a prior, and assumes you are 3 times more confident than
+    // a raw beta would be. This gives your distribution more of a peak for small numbers.
+    return { values: Sample(n, () => jStat.beta.sample(3*hits, 3*(total-hits))) }
   }
 }
 
