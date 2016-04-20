2dd5ba31a4899e52304c1a4613b07925ca455cb9
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-20 09:34:03 -0700

    Cleaning up code a bit, removing unneeded dependency.

--- src/lib/guesstimator/formatter/formatters/DistributionTextProportion.js
+++ src/lib/guesstimator/formatter/formatters/DistributionTextProportion.js
@@ -1,21 +1,27 @@
-import {confidenceIntervalTextMixin} from './lib.js'
+import {parseNumber} from './lib.js'
 
-export const item = Object.assign(
-  {}, confidenceIntervalTextMixin,
-  {
-    inputType: 'TEXT',
-    formatterName: 'DISTRIBUTION_PROPORTIONALITY',
+export const item = {
+  inputType: 'TEXT',
+  formatterName: 'DISTRIBUTION_PROPORTIONALITY',
 
-    errors(g) { return [] },
-
-    matches({text}) { return !!text && _.isString(text) && text.includes(" of ") },
-
-    _numbers(text) { return this._splitNumbersAt(text, " of ") },
-
-    format(g) {
-      const [hits, total] = this._numbers(g.text)
-      const guesstimateType = "BETA"
-      return {guesstimateType, hits, total}
+  errors(g) {
+    const numbers = this._numbers(g.text)
+    if (numbers.length !== 2) {
+      return ['only accepts two numbers']
+    } else if (numbers[1] < numbers[0]) {
+      return ['the second number must be greater than the first number']
     }
+    return []
+  },
+
+  matches({text}) { return !!text && _.isString(text) && text.includes(" of ") },
+
+  _numbers(text) { return text.split(" of ").map(e => parseNumber(e.trim())) },
+
+  format(g) {
+    const [hits, total] = this._numbers(g.text)
+    const guesstimateType = "BETA"
+    return {guesstimateType, hits, total}
   }
-)
+}
+
