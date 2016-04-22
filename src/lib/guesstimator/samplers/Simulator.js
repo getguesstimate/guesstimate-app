368b450d17e73ea492f4bebc30920e3b5f6bf434
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-22 13:54:46 -0700

    4 WebWorkers > 1 WebWorker.

--- src/lib/guesstimator/samplers/Simulator.js
+++ src/lib/guesstimator/samplers/Simulator.js
@@ -1,15 +1,25 @@
 import Worker from 'worker!./simulator-worker/index.js'
-const worker = new Worker
+const workers = [new Worker, new Worker, new Worker, new Worker]
 
 export const simulate = (expr, inputs, numSamples) => {
+  const data = {expr, numSamples: numSamples/4, inputs}
+  return Promise.all(workers.map(worker => simulateOnWorker(worker, data))).then(
+    (results) => {
+      let finalResult = {values: [], errors: []}
+      for (let result of results) {
+        finalResult.values = finalResult.values.concat(result.values)
+        finalResult.errors = finalResult.errors.concat(result.errors)
+      }
+      return finalResult
+    }
+  )
+}
+
+const simulateOnWorker = (worker, data) => {
   return new Promise(
     (resolve, reject) => {
-      worker.onmessage = ({data}) => resolve(JSON.parse(data))
-      worker.postMessage(JSON.stringify({
-        expr,
-        numSamples,
-        inputs
-      }))
+      worker.onmessage = ({data}) => {resolve(JSON.parse(data))}
+      worker.postMessage(JSON.stringify(data))
     }
   )
 }
