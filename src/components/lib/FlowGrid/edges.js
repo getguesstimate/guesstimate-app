a81d1fbfcdc59395f29c1db2f7ed4e61c43440dd
Ozzie Gooen <ozzieagooen@gmail.com>
2016-04-24 20:59:47 -0700

    Reduced padding

--- src/components/lib/FlowGrid/edges.js
+++ src/components/lib/FlowGrid/edges.js
@@ -3,7 +3,7 @@ import Edge from './edge';
 import _ from 'lodash'
 
 let upto = (n) => Array.apply(null, {length: n}).map(Number.call, Number)
-const PADDING_WIDTH = 7
+const PADDING_WIDTH = 6
 
 export default class Edges extends Component {
   displayName: 'Edges'
