691bc60aa64caa5b7b4c31d9c2b2e39602c968ee
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-25 18:54:34 -0700

    remove from standard TextUpTo formatter.

--- src/lib/guesstimator/formatter/formatters/DistributionTextUpTo.js
+++ src/lib/guesstimator/formatter/formatters/DistributionTextUpTo.js
@@ -5,7 +5,7 @@ export const item = Object.assign(
   {
     inputType: 'TEXT',
     formatterName: 'DISTRIBUTION_NORMAL_TEXT_UPTO',
-    _symbols: ['->', ':', '..', ' to '],
+    _symbols: ['->', ':', '..'],
     _numbers(text) { return this._splitNumbersAt(text, this._relevantSymbol(text)) },
     format(g) {
       const [low, high] = this._numbers(g.text)
