5fad621416c7d306d2ae5d049c91b40ecffa8aca
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-26 17:01:54 -0700

    Moving onlyHead check back to the top for performance reasons, then also returning the head node in the notHead case if there is an infinite loop

--- src/lib/engine/graph.js
+++ src/lib/engine/graph.js
@@ -45,6 +45,10 @@ export function dependencyList(graph, spaceId) {
 
 // This could be optimized for filtering the graph by the space subset
 export function dependencyTree(oGraph, graphFilters) {
+  if (onlyHead) {
+    return [[metricId,0]]
+  }
+
   const {spaceId, metricId, onlyHead, notHead} = graphFilters
   let graph = oGraph
   if (spaceId) { graph = _space.subset(oGraph, spaceId) }
@@ -53,10 +57,14 @@ export function dependencyTree(oGraph, graphFilters) {
   if (metricId) { bGraph = bGraph.subsetFrom(metricId) }
 
   const nodes = bGraph.nodes.map(n => [n.id, n.maxDistanceFromRoot])
-  if (onlyHead) {
-    return nodes.filter(e => (e[0] === metricId))
-  } else if (notHead){
-    return nodes.filter(e => (e[0] !== metricId))
+
+  if (notHead){
+    const head = nodes.find(e => (e[0] === metricId))
+    const rest = nodes.filter(e => (e[0] !== metricId))
+    if (!_.isFinite(head[1])) {
+      return [head,...rest]
+    }
+    return rest
   } else {
     return nodes
   }
