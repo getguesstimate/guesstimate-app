cb61f310a632abf808ec0d5df1f03e59d4e08baf
Matthew McDermott <mattmcdermott8@gmail.com>
2016-04-29 18:45:18 -0700

    Selective shared rendering methods for metric card cell updating.

---
+++ src/components/metrics/card/updated.js
@@ -0,0 +1,10 @@
+export function hasMetricUpdated(oldProps, newProps) {
+  return (
+    oldProps.isSelected !== newProps.isSelected ||
+    oldProps.canvasState.metricCardView !== newProps.canvasState.metricCardView ||
+    oldProps.canvasState.metricClickMode !== newProps.canvasState.metricClickMode ||
+    oldProps.canvasState.saveState !== newProps.canvasState.saveState ||
+    !!oldProps.metric.simulation !== !!newProps.metric.simulation ||
+    (!!oldProps.metric.simulation && (oldProps.metric.simulation.propagationId !== newProps.metric.simulation.propagationId))
+  )
+}
