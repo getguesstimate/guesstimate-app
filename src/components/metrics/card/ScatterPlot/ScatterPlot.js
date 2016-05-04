a537895151466aa4aeb6fbe0146b173ddbbf9fab
Ozzie Gooen <ozzieagooen@gmail.com>
2016-05-04 13:35:26 -0700

    Separated Scatterplot into multiple components

--- src/components/metrics/card/ScatterPlot/ScatterPlot.js
+++ src/components/metrics/card/ScatterPlot/ScatterPlot.js
@@ -10,75 +10,15 @@ function importance(r2) {
   else { return 'high' }
 }
 
-export default class Foobar extends Component {
-  static defaultProps = {
-    size: 'SMALL'
-  }
-
+class Plot extends Component {
   render() {
-    const {xMetric, yMetric} = this.props
+    const {xSamples, ySamples, size, xLabel, yLabel} = this.props
+    const data = [{ customValues: _.zip(xSamples, ySamples) }];
+    const valuesAccessor = (s) => s.customValues
+    const xAccessor = (e) => e[0]
+    const yAccessor = (e) => e[1]
 
-    let sampleSize = (this.props.size === 'SMALL') ? 100 : 1000
-
-    const xSamples = _.get(xMetric, 'simulation.sample.values').slice(0, sampleSize)
-    const ySamples = _.get(yMetric, 'simulation.sample.values').slice(0, sampleSize)
-
-    const data = [{
-      customValues: _.zip(xSamples, ySamples)
-    }];
-
-    let regression = !!xSamples.length ? everpolate.linearRegression(xSamples, ySamples) : false
-
-    var labelAccessor = (s) => ''
-    var valuesAccessor = (s) => s.customValues
-    var xAccessor = (e) => e[0]
-    var yAccessor = (e) => e[1]
-
-    const tooltipScatter = (x,y) => ""
-    const className=`Scatter ${this.props.size}`
-
-    const rSquared = regression.rSquared
-    const xIntercept = regression.intercept && (-1 * (regression.intercept / regression.slope))
-    return (
-      <div className={className}>
-        <div className='regression'>
-          {this.props.size === 'SMALL' && _.isFinite(rSquared) &&
-            <div>
-              <span className='label'> r<sup>2</sup></span>
-              <span className={`value ${importance(rSquared)}`}> {rSquared.toFixed(2)}</span>
-            </div>
-          }
-          {this.props.size !== 'SMALL' &&
-            <div>
-              <div>
-                <span className='label'> r<sup>2</sup></span>
-                <span className='value'> {regression.rSquared.toFixed(2)}</span>
-              </div>
-              <div>
-                <span className='label'> slope</span>
-                <span className='value'> {regression.slope.toFixed(2)}</span>
-              </div>
-              <div>
-                <span className='label'> x intercept</span>
-                <span className='value'> {xIntercept.toFixed(2)}</span>
-              </div>
-              <div>
-                <span className='label'> y intercept</span>
-                <span className='value'> {regression.intercept.toFixed(2)}</span>
-              </div>
-              <div>
-                <span className='label'> x intercept</span>
-                <span className='value'> {xIntercept.toFixed(2)}</span>
-              </div>
-              <div>
-                <span className='label'> sample count</span>
-                <span className='value'> {sampleSize}</span>
-              </div>
-            </div>
-          }
-        </div>
-
-        {this.props.size === 'SMALL' &&
+    if (size === 'SMALL'){ return(
           <ScatterPlot
             data={data}
             width={180}
@@ -89,19 +29,83 @@ export default class Foobar extends Component {
             x={xAccessor}
             y={yAccessor}
             values={valuesAccessor}/>
+    )}
+    else { return(
+      <ScatterPlot
+        data={data}
+        width={500}
+        height={300}
+        margin={{top: 10, bottom: 40, left: 60, right: 20}}
+        xAxis={{tickArguments: [6], innerTickSize: 5, outerTickSize: 2, tickPadding: 3, label: xLabel}}
+        yAxis={{tickArguments: [6], innerTickSize: 5, outerTickSize: 2, tickPadding: 3, label: yLabel}}
+        x={xAccessor}
+        y={yAccessor}
+        values={valuesAccessor}/>
+    )}
+  }
+}
+
+class RegressionStats extends Component {
+  render() {
+    const {xSamples, ySamples, size} = this.props
+    const regression = !!xSamples.length ? everpolate.linearRegression(xSamples, ySamples) : false
+    const sampleSize = xSamples && xSamples.length
+
+    const rSquared = regression.rSquared
+    const xIntercept = regression.intercept && (-1 * (regression.intercept / regression.slope))
+    return (
+      <div className='regression'>
+        {size === 'SMALL' && _.isFinite(rSquared) &&
+          <div>
+            <span className='label'> r<sup>2</sup></span>
+            <span className={`value ${importance(rSquared)}`}> {rSquared.toFixed(2)}</span>
+          </div>
         }
-        {this.props.size !== 'SMALL' &&
-          <ScatterPlot
-            data={data}
-            width={500}
-            height={300}
-            margin={{top: 10, bottom: 40, left: 60, right: 20}}
-            xAxis={{tickArguments: [6], innerTickSize: 5, outerTickSize: 2, tickPadding: 3, label: xMetric.name}}
-            yAxis={{tickArguments: [6], innerTickSize: 5, outerTickSize: 2, tickPadding: 3, label: yMetric.name}}
-            x={xAccessor}
-            y={yAccessor}
-            values={valuesAccessor}/>
+        { size !== 'SMALL' &&
+          <div>
+            <div>
+              <span className='label'> r<sup>2</sup></span>
+              <span className='value'> {regression.rSquared.toFixed(2)}</span>
+            </div>
+            <div>
+              <span className='label'> slope</span>
+              <span className='value'> {regression.slope.toFixed(2)}</span>
+            </div>
+            <div>
+              <span className='label'> x intercept</span>
+              <span className='value'> {xIntercept.toFixed(2)}</span>
+            </div>
+            <div>
+              <span className='label'> y intercept</span>
+              <span className='value'> {regression.intercept.toFixed(2)}</span>
+            </div>
+            <div>
+              <span className='label'> sample count</span>
+              <span className='value'> {sampleSize}</span>
+            </div>
+          </div>
         }
+      </div>
+    )
+  }
+}
+
+export default class ScatterRegion extends Component {
+  static defaultProps = {
+    size: 'SMALL'
+  }
+
+  render() {
+    const {xMetric, yMetric, size} = this.props
+
+    let sampleSize = (size === 'SMALL') ? 100 : 1000
+    const xSamples = _.get(xMetric, 'simulation.sample.values').slice(0, sampleSize)
+    const ySamples = _.get(yMetric, 'simulation.sample.values').slice(0, sampleSize)
+
+    return (
+      <div className={`Scatter ${this.props.size}`}>
+        <RegressionStats xSamples={xSamples} ySamples={ySamples} size={size}/>
+        <Plot xSamples={xSamples} ySamples={ySamples} size={size} xLabel={xMetric.name} yLabel={yMetric.name}/>
       </div>
     )
   }
