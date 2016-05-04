a72336eedd02ab6004f41a6702ef1eb651a839ab
Ozzie Gooen <OAGr@users.noreply.github.com>
2016-05-04 11:41:39 -0700

    Revert "Selective shared rendering methods for metric card cell updating."

--- src/components/spaces/canvas/index.js
+++ src/components/spaces/canvas/index.js
@@ -10,8 +10,6 @@ import { changeMetric, addMetric } from 'gModules/metrics/actions'
 import { changeSelect } from 'gModules/selection/actions'
 import { runSimulations, deleteSimulations } from 'gModules/simulations/actions'
 
-import { hasMetricUpdated } from 'gComponents/metrics/card/updated.js'
-
 import './style.css'
 import { denormalizedSpaceSelector } from '../denormalized-space-selector.js';
 import JSONTree from 'react-json-tree'
@@ -86,6 +84,13 @@ export default class CanvasSpace extends Component{
     this.props.dispatch(changeSelect(next))
   }
 
+  _hasMetricUpdated(oldProps, newProps) {
+    return (
+      oldProps.canvasState !== newProps.canvasState ||
+      oldProps.metric.simulation !== newProps.metric.simulation
+    )
+  }
+
   renderMetric(metric) {
     const {location} = metric
     return (
@@ -139,7 +144,7 @@ export default class CanvasSpace extends Component{
         }
         <FlowGrid
           items={metrics.map(m => ({key: m.id, location: m.location, component: this.renderMetric(m)}))}
-          hasItemUpdated = {(oldItem, newItem) => hasMetricUpdated(oldItem.props, newItem.props)}
+          hasItemUpdated = {(oldItem, newItem) => this._hasMetricUpdated(oldItem.props, newItem.props)}
           edges={edges}
           selected={selected}
           onSelectItem={this._handleSelect.bind(this)}
