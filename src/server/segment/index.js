2739a3351c546a88d1ce19001003c3aec90f4221
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-05 10:52:11 -0700

    fixing segment constant specification.

--- src/server/segment/index.js
+++ src/server/segment/index.js
@@ -1,8 +1,8 @@
-import * as EventTypes from './constants.js'
+import * as Constants from './constants.js'
 
 export function initialize() {
     !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
-    analytics.load(API_KEY);
+    analytics.load(Constants.API_KEY);
   }}()
 }
 
@@ -21,25 +21,25 @@ function segmentPlanType(rawPlanType) {
 }
 
 function trackAddedProduct(planType) {
-  window.analytics.track(EventTypes.ADDED_PRODUCT_ACTION, {
+  window.analytics.track(Constants.ADDED_PRODUCT_ACTION, {
     id: planType,
     name: planType
   })
 }
 
 function trackViewedAllProducts() {
-  window.analytics.track(EventTypes.VIEWED_PRODUCT_ACTION, {
+  window.analytics.track(Constants.VIEWED_PRODUCT_ACTION, {
     id: LITE_PLAN,
     name: LITE_PLAN
   })
-  window.analytics.track(EventTypes.VIEWED_PRODUCT_ACTION, {
+  window.analytics.track(Constants.VIEWED_PRODUCT_ACTION, {
     id: PREMIUM_PLAN,
     name: PREMIUM_PLAN
   })
 }
 
 export function trackUsedViewMode(mode) {
-  window.analytics.track(EventTypes.USED_VIEW_MODE, {mode})
+  window.analytics.track(Constants.USED_VIEW_MODE, {mode})
 }
 
 const pageEvents = {
@@ -63,7 +63,7 @@ export function trackUser(userId, info) {
 export function trackPurchaseSuccess(account, rawPlanType) {
   const planType = segmentPlanType(rawPlanType)
   const orderId = `${account.id}-${Date.now()}` // The unique order for this account finishing right now.
-  window.analytics.track(EventTypes.PURCHASE_SUCCESS_ACTION, {
+  window.analytics.track(Constants.PURCHASE_SUCCESS_ACTION, {
     orderId: orderId,
     products: [
       {
@@ -75,13 +75,13 @@ export function trackPurchaseSuccess(account, rawPlanType) {
 }
 
 export function trackAccountModalClick() {
-  window.analytics.track(EventTypes.ACCOUNT_MODAL_CLICK)
+  window.analytics.track(Constants.ACCOUNT_MODAL_CLICK)
 }
 
 export function trackUserMenuOpen() {
-  window.analytics.track(EventTypes.USER_MENU_OPEN)
+  window.analytics.track(Constants.USER_MENU_OPEN)
 }
 
 export function trackUserMenuClose() {
-  window.analytics.track(EventTypes.USER_MENU_CLOSE)
+  window.analytics.track(Constants.USER_MENU_CLOSE)
 }
