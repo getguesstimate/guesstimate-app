b5a00ad8bcb97ecfd783bedd7c96cdc982a9bcc3
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-09 12:57:08 -0700

    Don't resample data and show data histograms for even small data sets.

--- src/lib/guesstimator/samplers/Data.js
+++ src/lib/guesstimator/samplers/Data.js
@@ -1,8 +1,6 @@
 export var Sampler = {
   sample(formatted, n) {
-    const {data} = formatted
-    const values = _.range(n).map(e => _.sample(data))
-    return Promise.resolve({values})
+    return Promise.resolve({values: formatted.data})
   }
 }
 
