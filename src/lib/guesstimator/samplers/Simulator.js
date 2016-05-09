f2fdc4c9f7333d0bd335583b94873386ad5b2514
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-09 16:24:57 -0700

    Proper number of samples. May do weird things for weird amounts of data.

--- src/lib/guesstimator/samplers/Simulator.js
+++ src/lib/guesstimator/samplers/Simulator.js
@@ -1,9 +1,33 @@
-export function simulate(expr, inputs, overallNumSamples) {
-  const numSamples = overallNumSamples/window.workers.length
-  return Promise.all(_.map(
-    window.workers,
-    (worker, index) => simulateOnWorker(worker, buildData(index, expr, numSamples, inputs))
-  )).then(
+import {STOCHASTIC_FUNCTIONS} from './simulator-worker/simulator/evaluator.js'
+
+const MIN_SAMPLES_PER_WINDOW = 100
+
+function GCD(a, b) {
+  return !b ? a : GCD(b, a % b)
+}
+
+function LCM(a, b) {
+  return (a * b) / GCD(a, b)
+}
+
+export function simulate(expr, inputs, maxSamples) {
+  const overallNumSamples = neededSamples(expr, inputs, maxSamples)
+  if (overallNumSamples < MIN_SAMPLES_PER_WINDOW*window.workers.length) {
+    return simulateOnWorker(window.workers[0], buildData(expr, 0, overallNumSamples, inputs))
+  }
+
+  const numSamples = Math.floor(overallNumSamples/window.workers.length)
+  const remainingSamples = numSamples + overallNumSamples % window.workers.length
+
+  const promises = [..._.map(
+      window.workers.slice(0,-1),
+      (worker, index) => simulateOnWorker(worker, buildData(expr, index*numSamples, numSamples, inputs))
+    ),
+    simulateOnWorker(window.workers[window.workers.length-1], buildData(expr, (window.workers.length - 1)* numSamples, remainingSamples, inputs))
+  ]
+
+
+  return Promise.all(promises).then(
     (results) => {
       let finalResult = {values: [], errors: []}
       for (let result of results) {
@@ -20,6 +44,23 @@ export function simulate(expr, inputs, overallNumSamples) {
   )
 }
 
+const hasStochasticFunction = text => _.some(STOCHASTIC_FUNCTIONS, e => text.indexOf(e) !== -1)
+
+function neededSamples(text, inputs, n){
+  if (hasStochasticFunction(text)) {
+    return n
+  }
+
+  const numInputs = Object.keys(inputs).map(key => inputs[key].length)
+  const lcm = numInputs.reduce((x,y) => LCM(x,y))
+  const product = numInputs.reduce((x,y) => x*y)
+  if (product < n) {
+    return product
+  } else {
+    return Math.min(n, lcm)
+  }
+}
+
 function modularSlice(array, from, to) {
   const len = array.length
   if (len <= to - from) {
@@ -32,10 +73,10 @@ function modularSlice(array, from, to) {
   return [...array.slice(newFrom), array.slice(0,to)]
 }
 
-function buildData(index, expr, numSamples, inputs) {
+function buildData(expr, prevModularIndex, numSamples, inputs) {
   let slicedInputs = {}
   for (let key of Object.keys(inputs)) {
-    slicedInputs[key] = modularSlice(inputs[key], numSamples*index, numSamples*(index+1))
+    slicedInputs[key] = modularSlice(inputs[key], prevModularIndex, prevModularIndex + numSamples)
   }
   return {expr, numSamples, inputs: slicedInputs}
 }
