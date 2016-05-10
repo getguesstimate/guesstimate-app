4bf957c42ab1cd73fbfa8ced031463f8eba2a4e4
Ozzie Gooen <ozzieagooen@gmail.com>
2016-05-09 18:30:17 -0700

    Updated Guesstimate name and description in metadata

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,21 @@ var elev = `<script> var _elev = window._elev || {};(function() {
       var i,e;i=document.createElement("script"),i.type='text/javascript';i.async=1,i.src="https://static.elev.io/js/v3.js",e=document.getElementsByTagName("script")[0],e.parentNode.insertBefore(i,e);})();
       _elev.account_id = '565e550e67ffc'</script>`
 
+var description = "Plan finances, make strategic decisions, and do risk assessment.  Guesstimate uses stochastic models, Monte Carlo simulations, and sensitivity analyses."
+var name = "Guesstimate | A Spreadsheet for the Uncertain"
 var wistia = `<script charSet="ISO-8859-1" src="//fast.wistia.com/assets/external/E-v1.js" async></script>`
 var twitter = `<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>`
 var fonts = `<link href='https://fonts.googleapis.com/css?family=Lato:400,700,300' rel='stylesheet' type='text/css'>`
-var googleTags = `<meta name='Description' content="A spreadsheet for things that aren't certain"/>`
+var googleTags = `<meta name='Description' content="${description}"/>`
 var chargebee = `<script type="text/javascript" src="https://js.chargebee.com/v1/chargebee.js"></script>`
 var ogtags = ''
              + `<meta property="og:type" content="product"/>`
-             + `<meta property="og:title" content="Guesstimate"/>`
-             + `<meta property="og:description" content="A spreadsheet for things that aren't certain"/>`
+             + `<meta property="og:title" content="${name}"/>`
+             + `<meta property="og:description" content="${description}"/>`
 
 var head = googleTags + ogtags + elev + wistia + twitter + fonts + chargebee
 
-var meta = {name: 'Guesstimate', content: `A spreadsheet for things that aren't certain`}
+var meta = {name: name, content: description}
 
 var cfg = getConfig({
   in: 'src/routes/app.js',
