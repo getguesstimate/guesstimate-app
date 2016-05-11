981ab2617cab64c419a7faa0c32fff4dd61a3087
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-11 15:15:14 -0700

    Not using drag drop approach atm

--- src/components/lib/FlowGrid/cell-empty.js
+++ src/components/lib/FlowGrid/cell-empty.js
@@ -1,48 +1,9 @@
 import React, {Component, PropTypes} from 'react'
-import {DragSource, DropTarget} from 'react-dnd'
 
-var cardSource = {
-  beginDrag: function (props) {
-    return {location: props.location}
-  },
-  endDrag: function(props, monitor) {
-    if (monitor.didDrop()){
-      const item = monitor.getItem();
-      const dropResult = monitor.getDropResult()
-      props.onMultipleSelect(item.location, dropResult.location)
-      //props.onEndDrag()
-    }
-  }
-}
-const squareTarget = {
-  //For partial highlight.
-  hover: function(props, monitor, component) {
-    props.onHoverSelect(monitor.getItem().location, props.location)
-  },
-  drop: function(props) {
-    return {location: props.location}
-  }
-}
-function collect(connect, monitor) {
-  return {
-    connectDropTarget: connect.dropTarget(),
-    isOver: monitor.isOver()
-  };
-}
-
-@DragSource('select', cardSource, (connect, monitor) => {
-  return ({
-    connectDragSource: connect.dragSource(),
-    isDragging: monitor.isDragging()
-  })
-})
-@DropTarget('select', squareTarget, collect)
 export default class EmptyCell extends Component {
   static propTypes = {
     gridKeyPress: PropTypes.func.isRequired,
     handleSelect: PropTypes.func.isRequired,
-    onHoverSelect: PropTypes.func,
-    isOver: PropTypes.bool.isRequired,
     isSelected: PropTypes.bool.isRequired,
     location: PropTypes.shape({
       row: PropTypes.number.isRequired,
@@ -67,7 +28,6 @@ export default class EmptyCell extends Component {
     if (e.button === 0){
       if (!this.props.isSelected) {
         this.props.handleSelect(this.props.location)
-        this.props.onMultipleSelect(this.props.location, this.props.location)
       } else {
         this.props.onAddItem(this.props.location)
       }
@@ -76,14 +36,14 @@ export default class EmptyCell extends Component {
 
   render() {
     let className = 'FlowGridEmptyCell grid-item-focus'
-    return this.props.connectDropTarget(this.props.connectDragSource(
+    return (
       <div
           className={className}
           onKeyDown={this._handleKeyPress.bind(this)}
           onMouseDown={this.handleClick.bind(this)}
           tabIndex='0'
       />
-    ))
+    )
   }
 }
 
