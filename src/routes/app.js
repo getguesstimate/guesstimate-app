fd17e3cce904cec2f2bec083db29b8cbc05a167d
Ozzie Gooen <ozzieagooen@gmail.com>
2016-05-13 11:09:42 -0700

    Selectively show elev

--- src/routes/app.js
+++ src/routes/app.js
@@ -5,6 +5,7 @@ import * as sentry from '../server/sentry/index.js'
 import engine from 'gEngine/engine.js'
 import {setupGuesstimateApi} from 'servers/guesstimate-api/constants.js'
 import './main.css'
+import * as elev from 'server/elev/index.js'
 
 import Worker from 'worker!../lib/guesstimator/samplers/simulator-worker/index.js'
 window.workers = [new Worker, new Worker]
@@ -39,6 +40,7 @@ app.extend({
       app_id: "o0trb1v9"
     };
     (function(){var w=window;var ic=w.Intercom;if(typeof ic==="function"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/o0trb1v9';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()
+    elev.hide()
     segment.initialize()
     sentry.initialize()
     this.router = new Router()
