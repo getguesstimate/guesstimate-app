ae0659c04f95959125bede9671ab7719ed855771
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-16 11:35:36 -0700

    A bit hacky, but the easiest definitive way to exclude ourselves from all further data analytics, regardless of provider.

--- src/server/segment/index.js
+++ src/server/segment/index.js
@@ -1,13 +1,14 @@
 import * as Constants from './constants.js'
 
+const LITE_PLAN = "lite"
+const PREMIUM_PLAN = "premium"
+const EMPLOYEE_USER_IDS = [240 /* Matthew Gmail */, 1 /* Ozzie Github */, 4227 /* Matthew Github */]
+
 export function initialize() {
     !function(){var analytics=window.analytics=window.analytics||[];if(!analytics.initialize)if(analytics.invoked)window.console&&console.error&&console.error("Segment snippet included twice.");else{analytics.invoked=!0;analytics.methods=["trackSubmit","trackClick","trackLink","trackForm","pageview","identify","reset","group","track","ready","alias","page","once","off","on"];analytics.factory=function(t){return function(){var e=Array.prototype.slice.call(arguments);e.unshift(t);analytics.push(e);return analytics}};for(var t=0;t<analytics.methods.length;t++){var e=analytics.methods[t];analytics[e]=analytics.factory(e)}analytics.load=function(t){var e=document.createElement("script");e.type="text/javascript";e.async=!0;e.src=("https:"===document.location.protocol?"https://":"http://")+"cdn.segment.com/analytics.js/v1/"+t+"/analytics.min.js";var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(e,n)};analytics.SNIPPET_VERSION="3.1.0";
     analytics.load(Constants.API_KEY);
   }}()
 }
-
-const LITE_PLAN = "lite"
-const PREMIUM_PLAN = "premium"
 
 // This function resolves any changes we make in the representation of plans into the unified language used across
 // Segment's commerce events.
@@ -57,7 +58,11 @@ export function pageLoad() {
 }
 
 export function trackUser(userId, info) {
-  window.analytics.identify(userId, info)
+  if (_.some(EMPLOYEE_USER_IDS, e => e == userId)) {
+    console.warn("Skipping data analytics via user ID.")
+  } else {
+    window.analytics.identify(userId, info)
+  }
 }
 
 export function trackPurchaseSuccess(account, rawPlanType) {
