580ae078434b64bd3e2c80fe3073fa04f4a39f59
Ozzie Gooen <ozzieagooen@gmail.com>
2016-05-23 22:42:54 -0700

    Added background color for copied region

--- src/modules/copied/reducer.js
+++ src/modules/copied/reducer.js
@@ -1,7 +1,9 @@
 export const copiedR = (state = null, action) => {
   switch (action.type) {
     case 'COPY':
-      return action.copied
+      return {...action.copied, pastedTimes: 0}
+    case 'COPY_PASTE':
+      return {...state, pastedTimes: (state.pastedTimes + 1)}
     default:
       return state
   }
