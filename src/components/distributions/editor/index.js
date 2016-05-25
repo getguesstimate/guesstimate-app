d27650b49ab0cf2ec88ac4624798ddac4a278e85
Matthew McDermott <mattmcdermott8@gmail.com>
2016-05-25 07:00:47 -0700

    Fixed function mode editor.

--- src/components/distributions/editor/index.js
+++ src/components/distributions/editor/index.js
@@ -23,7 +23,6 @@ export default class GuesstimateForm extends Component{
     guesstimateForm: PropTypes.object.isRequired,
     metricId: PropTypes.string.isRequired,
     metricFocus: PropTypes.func.isRequired,
-    editable: PropTypes.bool.isRequired,
     size: PropTypes.string,
     onOpen: PropTypes.func,
   }
@@ -71,7 +70,6 @@ export default class GuesstimateForm extends Component{
             size={size}
             hasErrors={errors && (errors.length !== 0)}
             ref='TextForm'
-            editable={this.props.editable}
           />
         }
       </div>
