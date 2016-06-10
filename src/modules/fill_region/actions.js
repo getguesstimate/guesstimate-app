bfe715fdd51d45e38e6cbaae968b807ee839b35b
Matthew McDermott <mattmcdermott8@gmail.com>
2016-06-10 13:17:14 -0700

    Functions.

--- src/modules/fill_region/actions.js
+++ src/modules/fill_region/actions.js
@@ -3,37 +3,38 @@ import {runSimulations} from 'gModules/simulations/actions'
 
 import e from 'gEngine/engine'
 
-import {isAtLocation, isWithinRegion, getBounds, move} from 'lib/locationUtils'
+import {isAtLocation, isWithinRegion, getBounds, move, translate} from 'lib/locationUtils'
 
-export function selectFillRegion(corner1, corner2) {
-  let leftX, topY, rightX, bottomY
-  leftX = Math.min(corner1.row, corner2.row)
-  topY = Math.max(corner1.column, corner2.column)
-  rightX = Math.max(corner1.row, corner2.row)
-  bottomY = Math.min(corner1.column, corner2.column)
-  return { type: 'SELECT_FILL_REGION', corner1: {row: leftX, column: bottomY}, corner2: {row: rightX, column: topY} }
+const DYNAMIC_FILL_TYPES = ['FUNCTION']
+
+// TODO(matthew): Dry up code with this and copy and undo perhaps.
+function translateReadableIds(input, idMap) {
+  if (!input) {return ""}
+  const re = RegExp(Object.keys(idMap).join("|"), "g")
+  return input.replace(re, (match) => idMap[match])
 }
-
-export function deSelectFillRegion() {
-  return { type: 'DE_SELECT_FILL_REGION' }
-}
-
-const CONSTANT_FILL_TYPES = ['POINT', 'NONE', 'DATA']
 
 const getDirAndLen = (start, end) => ({
   direction: {row: Math.sign(end.row - start.row), column: Math.sign(end.column - start.column)},
   length: Math.abs(start.row - end.row) || Math.abs(start.column - end.column),
 })
 
-function fillFunction(startMetric, startGuesstimate, metrics, guesstimates, idMap, newLocation) {
+function fillFunction(startMetric, startGuesstimate, metrics, idMap2, newLocation) {
+  const metric = {...startMetric, ...e.metric.create(Object.keys(idMap2)), location: newLocation}
+  const {input} = startGuesstimate
+  const translateFn = translate(startMetric.location, newLocation)
+  let idMap = {}
+  metrics.forEach(m => {
+    const matchedMetric = metrics.find(m2 => isAtLocation(translateFn(m.location), m2.location))
+    if (!!matchedMetric) { idMap[m.readableId] = matchedMetric.readableId }
+  })
+
+  return { metric, guesstimate: {...startGuesstimate, metric: metric.id, input: translateReadableIds(input, idMap)} }
 }
 
-function addAtLoc(startMetric, startGuesstimate, metrics, guesstimates, idMap, newLocation) {
+function addAtLoc(startMetric, startGuesstimate, metrics, idMap, newLocation) {
   const metric = {...startMetric, ...e.metric.create(Object.keys(idMap)), location: newLocation}
-  return {
-    metric,
-    guesstimate: {...startGuesstimate, metric: metric.id},
-  }
+  return { metric, guesstimate: {...startGuesstimate, metric: metric.id} }
 }
 
 function buildNewMetrics(startMetric, startGuesstimate, startLocation, {direction, length}, metrics, guesstimates) {
@@ -42,17 +43,17 @@ function buildNewMetrics(startMetric, startGuesstimate, startLocation, {directio
   let newMetrics = []
   let newGuesstimates = []
 
-  const translateFn = CONSTANT_FILL_TYPES.includes(guesstimateType) ? 
-    (loc, metrics, guesstimates, idMap) => addAtLoc(startMetric, startGuesstimate, metrics, guesstimates, idMap, loc)
+  const translateFn = DYNAMIC_FILL_TYPES.includes(guesstimateType) ? 
+    (loc, metrics, idMap) => fillFunction(startMetric, startGuesstimate, metrics, idMap, loc)
       :
-    (loc, metrics, guesstimates, idMap) => fillFunction(startMetric, startGuesstimate, metrics, guesstimates, idMap, loc)
+    (loc, metrics, idMap) => addAtLoc(startMetric, startGuesstimate, metrics, idMap, loc)
 
   let idMap = {}
   metrics.forEach(m => {idMap[m.readableId] = m.id})
 
   let currLocation = move(startLocation, direction)
   for (var i = 0; i < length; i++) {
-    const {metric, guesstimate} = translateFn(currLocation, metrics.concat(newMetrics), guesstimates.concat(newGuesstimates), idMap)
+    const {metric, guesstimate} = translateFn(currLocation, metrics.concat(newMetrics), idMap)
     idMap[metric.readableId] = metric.id
     newMetrics.push(metric)
     newGuesstimates.push(guesstimate)
@@ -71,7 +72,7 @@ export function fillRegion(spaceId, {start, end}) {
 
     const startMetric = metrics.find(m => isAtLocation(m.location, start))
 
-    const containedMetrics = metrics.filter(m => m.id !== startMetric.id && isWithinRegion(m.location, getBounds({start, end})))
+    const containedMetrics = metrics.filter(m => (!startMetric || m.id !== startMetric.id) && isWithinRegion(m.location, getBounds({start, end})))
     dispatch(metricActions.removeMetrics(containedMetrics.map(m => m.id)))
 
     if (!startMetric) { return }
