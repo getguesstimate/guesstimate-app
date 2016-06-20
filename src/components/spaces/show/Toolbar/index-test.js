fd0588343378470d8cd58cd63da52ba4699c66da
Matthew McDermott <mattmcdermott8@gmail.com>
2016-06-20 16:41:58 -0700

    Using enzyme

--- src/components/spaces/show/Toolbar/index-test.js
+++ src/components/spaces/show/Toolbar/index-test.js
@@ -1,13 +1,60 @@
 import React from 'react'
-import TestUtils from 'react-addons-test-utils'
-import {shallow} from 'enzyme'
+import Icon from 'react-fa'
+
+import {shallow, mount} from 'enzyme'
 import {TestRoot} from 'gComponents/rootTestComponent'
+import DropDown from 'gComponents/utility/drop-down/index'
 
 import {SpaceToolbar} from './index'
 
+import CanvasViewForm from '../canvasViewForm'
+
+/*
+      <SpaceToolbar
+        editableByMe={}
+        actionState={}
+        isLoggedIn={}
+        onCopyModel={}
+        onCopyMetrics={}
+        onPasteMetrics={}
+        onCutMetrics={}
+        onDestroy={}
+        onUndo={}
+        canUndo={}
+        onRedo={}
+        canRedo={}
+        editsAllowed={}
+        onAllowEdits={}
+        onForbidEdits={}
+      />
+      */
+
 describe('<SpaceToolbar>', () => {
-  it ('renders the view form', () => {
+  it ('Always shows the view form', () => {
     const toolbar = shallow(<SpaceToolbar />)
-    expect(toolbar.find('.SpaceShowToolbar')).to.have.length(1)
+    expect(toolbar.containsMatchingElement(<CanvasViewForm/>)).to.equal(true)
+  })
+
+  it ('Disables the undo/redo buttons when they are unavailable', () => {
+    const toolbar = shallow(
+      <SpaceToolbar
+        canUndo={false}
+        canRedo={false}
+      />
+    )
+    expect(toolbar.find('.header-action')).to.have.length(5)
+    expect(toolbar.find('.header-action.disabled')).to.have.length(2)
+  })
+
+  it ('Renders the file menu if you are logged in', () => {
+    let toolbar = shallow(<SpaceToolbar isLoggedIn={false} />)
+    expect(toolbar.containsMatchingElement(
+      <DropDown openLink={<a className='header-action'>File</a>} />
+    )).to.equal(false)
+
+    let toolbar = shallow(<SpaceToolbar isLoggedIn={true} />)
+    expect(toolbar.containsMatchingElement(
+      <DropDown openLink={<a className='header-action'>File</a>} />
+    )).to.equal(true)
   })
 })
