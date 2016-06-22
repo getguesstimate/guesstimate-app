537cc784980c06f9a7d84f58d3577e9f36d500c9
Matthew McDermott <mattmcdermott8@gmail.com>
2016-06-22 15:43:52 -0700

    Added 'See the calculations' link to the calculator view.

--- src/components/calculators/show.js
+++ src/components/calculators/show.js
@@ -5,21 +5,24 @@ import {bindActionCreators} from 'redux'
 import ReactMarkdown from 'react-markdown'
 import Helmet from 'react-helmet'
 
+import Container from 'gComponents/utility/container/Container.js'
 import {Input} from './input'
 import {Output} from './output'
 
 import {calculatorSpaceSelector} from './calculator-space-selector'
 
+import {navigate} from 'gModules/navigation/actions'
 import {fetchById} from 'gModules/calculators/actions'
 import {runSimulations} from 'gModules/simulations/actions'
 import {changeGuesstimate} from 'gModules/guesstimates/actions'
 
+import * as Space from 'gEngine/space'
+
 import {Guesstimator} from 'lib/guesstimator/index'
-import Container from 'gComponents/utility/container/Container.js'
 
 import './style.css'
 
-@connect(calculatorSpaceSelector, dispatch => bindActionCreators({fetchById, changeGuesstimate, runSimulations}, dispatch))
+@connect(calculatorSpaceSelector, dispatch => bindActionCreators({navigate, fetchById, changeGuesstimate, runSimulations}, dispatch))
 export class CalculatorShow extends Component {
   state = {
     attemptedFetch: false,
@@ -55,59 +58,67 @@ export class CalculatorShow extends Component {
   render() {
     if (!this.props.calculator) { return false }
 
-    const {calculator: {content, title}, inputs, outputs} = this.props
+    const {calculator: {content, title, space_id}, inputs, outputs, navigate} = this.props
+    const spaceUrl = Space.url({id: space_id})
     return (
       <Container>
-        <div>
-          <Helmet
-            title={title}
-            meta={[
-              {name: "Description", content},
-              {property: "og:description", content},
-              {property: "og:title", content: title},
-              {property: "og:site_name", content: "Guesstimate"},
-            ]}
-          />
-          <div className='row'>
-            <div className='col-md-3'/>
-            <div className='col-md-6'>
-              <div className='calculator'>
-                <h1>{title}</h1>
-                <div className='description'>
-                  <ReactMarkdown source={content} />
-                </div>
-                <div className='inputs'>
-                  {_.map(inputs, (metric, i) => (
-                    <Input key={i} name={metric.name} onChange={this.onChange.bind(this, metric)}/>
-                  ))}
-                </div>
-                {this.state.showResult &&
-                  <div>
-                    <hr className='result-divider'/>
-                    <div className='outputs'>
-                      {_.map(outputs, (m, i) => <Output key={i} metric={m}/>)}
-                    </div>
+        <Helmet
+          title={title}
+          meta={[
+            {name: "Description", content},
+            {property: "og:description", content},
+            {property: "og:title", content: title},
+            {property: "og:site_name", content: "Guesstimate"},
+          ]}
+        />
+        <div className='row'>
+          <div className='col-md-3'/>
+          <div className='col-md-6'>
+            <div className='calculator'>
+              <h1>{title}</h1>
+              <div className='description'>
+                <ReactMarkdown source={content} />
+              </div>
+              <div className='inputs'>
+                {_.map(inputs, (metric, i) => (
+                  <Input key={i} name={metric.name} onChange={this.onChange.bind(this, metric)}/>
+                ))}
+              </div>
+              {this.state.showResult &&
+                <div>
+                  <hr className='result-divider'/>
+                  <div className='outputs'>
+                    {_.map(outputs, (m, i) => <Output key={i} metric={m}/>)}
                   </div>
-                }
-                {!this.state.showResult &&
                   <div className='row'>
-                    <div className='col-md-7' />
-                    <div
-                      className='col-md-5'
-                    >
-                      <div
-                        className='ui button green calculateButton'
-                        onClick={() => {this.setState({showResult: true})}}
-                      >
-                        Calculate
+                    <div className='col-md-3'/>
+                    <div className='col-md-6'>
+                      <div className='model-link'>
+                        <a href={spaceUrl} onClick={navigate.bind(spaceUrl)}>See the calculations</a>
                       </div>
                     </div>
+                    <div className='col-md-3'/>
                   </div>
-                }
-              </div>
+                </div>
+              }
+              {!this.state.showResult &&
+                <div className='row'>
+                  <div className='col-md-7' />
+                  <div
+                    className='col-md-5'
+                  >
+                    <div
+                      className='ui button green calculateButton'
+                      onClick={() => {this.setState({showResult: true})}}
+                    >
+                      Calculate
+                    </div>
+                  </div>
+                </div>
+              }
             </div>
-            <div className='col-md-3' />
           </div>
+          <div className='col-md-3' />
         </div>
       </Container>
     )
