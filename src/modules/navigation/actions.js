6f307f5f447984a09578c1f17ae059bc83d6ab48
Matthew McDermott <mattmcdermott8@gmail.com>
2016-06-24 13:15:08 -0700

    fixing the navigation bug.

--- src/modules/navigation/actions.js
+++ src/modules/navigation/actions.js
@@ -3,3 +3,5 @@ import app from 'ampersand-app'
 export function navigate(url) {
   if (url) { app.router.history.navigate(url)}
 }
+
+export function navigateFn(url) { return () => {navigate(url)} }
