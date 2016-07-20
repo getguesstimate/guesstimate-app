eb601e92811e46a8802d0a045f27a8d20ffefd50
Matthew McDermott <mattmcdermott8@gmail.com>
2016-07-20 16:24:10 -0700

    The delete button in the compressed view should work.

--- src/lib/guesstimate_api/resources/Calculators.js
+++ src/lib/guesstimate_api/resources/Calculators.js
@@ -8,6 +8,13 @@ export class Calculators extends AbstractResource {
     this.guesstimateMethod({url, method})(callback)
   }
 
+  destroy(id, callback) {
+    const url = `calculators/${id}`
+    const method = 'DELETE'
+
+    this.guesstimateMethod({url, method})(callback)
+  }
+
   create(spaceId, data, callback) {
     const url = `spaces/${spaceId}/calculators`
     const method = 'POST'
