9224303e5d3fa788b713200bdc47ffec1de126dd
Ozzie Gooen <ozzieagooen@gmail.com>
2016-08-02 13:28:07 -0700

    Basic first attempt at new pill fact styling

--- src/components/organizations/show/facts/factRow.js
+++ src/components/organizations/show/facts/factRow.js
@@ -2,30 +2,43 @@ import React, {Component} from 'react'
 
 import Icon from 'react-fa'
 
+import {DistributionSummary} from 'gComponents/distributions/summary/index'
 import Histogram from 'gComponents/simulations/histogram/index'
 
 import './facts.css'
 
 export const FactRow = ({fact, onEdit}) => (
   <div className='Fact'>
-    <div className='row'>
-      <div className='col-md-3'>
-        <Histogram
-          height={30}
-          simulation={fact.simulation}
-          cutOffRatio={0.995}
+    <div className='section-simulation simulation-sample'>
+      {_.has(fact, 'simulation.sample.values.length') && _.has(fact, 'simulation.stats.mean') &&
+        <div className='simulation-summary'>
+        <DistributionSummary
+          length={fact.simulation.sample.values.length}
+          mean={fact.simulation.stats.mean}
+          adjustedConfidenceInterval={fact.simulation.stats.adjustedConfidenceInterval}
         />
-      </div>
-      <div className='col-md-6'><span className='name'>{fact.name}</span></div>
-      <div className='col-md-2'>
-        <div className='variableName'>
-          <span className='prefix'>#</span>
-          <span className='variable'>{fact.variable_name}</span>
         </div>
+       }
+      {_.has(fact, 'simulation.sample.values.length') &&
+        <div className='histogram'>
+          <Histogram
+            height={15}
+            simulation={fact.simulation}
+            cutOffRatio={0.995}
+          />
+        </div>
+      }
+    </div>
+    <div className='section-name'>
+      <div className='variableName'>
+        <span className='prefix'>#</span>
+        <span className='variable'>{fact.variable_name}</span>
       </div>
-      <div className='col-md-1'>
-        <span className='ui button options' onClick={onEdit}>Edit</span>
-      </div>
+      <span className='name'>{fact.name}</span>
+    </div>
+
+    <div className='section-help'>
+      <span className='ui button options' onClick={onEdit}>Edit</span>
     </div>
   </div>
 )
