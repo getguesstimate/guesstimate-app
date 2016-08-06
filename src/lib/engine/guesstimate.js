1a05dace9012914157a5e45c787d479b44ef35ec
Matthew McDermott <mattmcdermott8@gmail.com>
2016-08-05 18:46:44 -0700

    Removed guesstimate errors.

--- src/lib/engine/guesstimate.js
+++ src/lib/engine/guesstimate.js
@@ -131,5 +131,6 @@ export function expressionToInputFn(metrics=[], facts=[]) {
 
 // Returns an expression based on the passed input and idMap.
 export function inputToExpression(input, idMap) {
+  if (_.isEmpty(input) || _.isEmpty(idMap)) { return input }
   return input.replace(RegExp(Object.keys(idMap).join('|'), 'g'), match => expressionSyntaxPad(idMap[match].id, idMap[match].isMetric))
 }
