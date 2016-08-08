f669dc674041be0c6c89ba7b280b88026cb6e504
Matthew McDermott <mattmcdermott8@gmail.com>
2016-08-07 20:14:01 -0700

    WIP

--- src/lib/recorder.js
+++ src/lib/recorder.js
@@ -1,6 +1,22 @@
 const incrementOrOne = (val) => { val = (val || 0) + 1 }
 const concatOrNewList = (list, val) => { list = (list || []).concat(val) }
 const getParentComponent = comp => _.get(comp, '_reactInternalInstance._currentElement._owner')
+// val of the form
+// { name, data, start, end, duration, children: [] }
+function addAtPosition(position, list, val) {
+  const list = getChildrenAtPosition(position, list)
+  parent.children.push(val)
+  return parent.children.length - 1
+}
+function getAtPosition(position, list) {
+  let el = list[position.shift()]
+  while (!_.isEmpty(position)) { el = el.children[position.shift()] }
+  return el
+}
+function getChildrenAtPosition(position, list) {
+  if (_.isEmpty(position)) { return list }
+  return getAtPosition(position, list).children}
+}
 
 export class GuesstimateRecorder {
   clearRecordings() {
@@ -32,78 +48,42 @@ export class GuesstimateRecorder {
     return parentIndices
   }
 
-  static addSingletonToNestedList(name, id, time, data, list) {
-    const lastElm = list[list.length - 1]
-    if (list.length === 0 || !!lastElm.end) {
-      list.push({name, id, time, data, end: true})
-    } else {
-      GuesstimateRecorder.addSingletonToNestedList(name, id, time, data, lastElm.children)
-    }
-  }
-  static addStartToNestedList(name, id, time, data, list) {
-    const lastElm = list[list.length - 1]
-    if (list.length === 0 || !!lastElm.end) {
-      list.push({name, id, start: time, children: [], data, end: null})
-    } else {
-      GuesstimateRecorder.addStartToNestedList(name, id, time, data, lastElm.children)
-    }
-  }
-  static addStopToNestedList(name, id, time, data, list) {
-    // TODO(matthew): Examine all children (async).
-    if (!list) {
-      console.warn("Failed to close timing for ", name, " at ", time)
-      return {duration: null}
-    }
-    const lastElm = list[list.length - 1]
-    if (lastElm.id === id) {
-      lastElm.end = time
-      lastElm.data = data
-      lastElm.duration = lastElm.end - lastElm.start
-      return lastElm
-    } else {
-      return GuesstimateRecorder.addStopToNestedList(name, id, time, data, lastElm.children)
-    }
-  }
-
-  recordNamedEvent(name, id, suffix = "", nestFn = GuesstimateRecorder.addSingletonToNestedList, data={}) {
-    if (this.disabled || this.paused) { return }
-    const time = (new Date()).getTime() - this.appStartTime
-    this.timeline = this.timeline.concat({name: `${name} ${suffix}`, id, time, data})
-    return nestFn(name, id, time, data, this.nestedTimeline)
-  }
-
   recordReductionEvent(action) {
-    if (this.disabled || this.paused) { return }
-    this.recordNamedEvent(action.type, null, " Reducing", GuesstimateRecorder.addSingletonToNestedList, action)
-    incrementOrOne(this.actionCounts[action.type])
+    //  if (this.disabled || this.paused) { return }
+    //  this.recordNamedEvent(action.type, null, " Reducing", GuesstimateRecorder.addSingletonToNestedList, action)
+    //  incrementOrOne(this.actionCounts[action.type])
   }
 
-  recordSelectorStart(name) { this.recordNamedEvent(name, null, " Start", GuesstimateRecorder.addStartToNestedList) }
+  recordSelectorStart(name) { } //this.recordNamedEvent(name, null, " Start", GuesstimateRecorder.addStartToNestedList) }
   recordSelectorStop(name, returned) {
-    if (this.disabled || this.paused) { return }
-    const fullSelector = this.recordNamedEvent(name, null, " Stop", GuesstimateRecorder.addStopToNestedList, returned)
-    incrementOrOne(this.selectorCounts[name])
-    concatOrNewList(this.selectorTimings[name], fullSelector.duration)
+    //  if (this.disabled || this.paused) { return }
+    //  const fullSelector = this.recordNamedEvent(name, null, " Stop", GuesstimateRecorder.addStopToNestedList, returned)
+    //  incrementOrOne(this.selectorCounts[name])
+    //  concatOrNewList(this.selectorTimings[name], fullSelector.duration)
   }
 
-  recordMountEvent(component) {
-    this.recordNamedEvent(`${component.constructor.name} Mount`)
-    component['__recorder_id__'] = this.uniqueId++
-  }
-  recordUnmountEvent(component) { this.recordNamedEvent(`${component.constructor.name} Unmount`) }
+  recordMountEvent(component) { }
+  //  this.recordNamedEvent(`${component.constructor.name} Mount`)
+  //  component['__recorder_id__'] = this.uniqueId++
+  //}
+  recordUnmountEvent(component) { } // this.recordNamedEvent(`${component.constructor.name} Unmount`) }
   recordRenderStartEvent(component) {
+    if (this.disabled || this.paused) { return }
+
     const parentIndices = gatherParentIndices(component)
+    const time = (new Date()).getTime() - this.appStartTime
 
-
-    this.recordNamedEvent(component.constructor.name, component['__recorder_id__'], " Render Start", GuesstimateRecorder.addStartToNestedList)
+    const element = {
+      name: component.constructor.name,
+      time,
+      children: []
+    }
+    component['__recorder_index__'] = addAtPosition(parentIndices, this.nestedTimeline, element)
   }
   recordRenderStopEvent(component) {
     if (this.disabled || this.paused) { return }
-
-    const name = component.constructor.name
-    const fullRender = this.recordNamedEvent(name, component['__recorder_id__'], " Render Stop", GuesstimateRecorder.addStopToNestedList)
-    incrementOrOne(this.renderCounts[name])
-    concatOrNewList(this.renderTimings[name], fullRender.duration)
+    const parentIndices = gatherParentIndices(component)
+    const time = (new Date()).getTime() - this.appStartTime
   }
   pause() { this.paused = true }
   unpause() { this.paused = false }
