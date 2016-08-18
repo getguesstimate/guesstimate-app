b86a5f641001f2a78e7079e30bb7cd9000cdc0ae
Matthew McDermott <mattmcdermott8@gmail.com>
2016-08-18 11:14:31 -0700

    Updating URL regex to be more comprehensive.

--- src/lib/engine/utils.js
+++ src/lib/engine/utils.js
@@ -1,6 +1,7 @@
 import * as _collections from './collections'
 
-const URL_REGEX = /(?:http(?:s?):\/\/)?(?:www)?\w+\.(?:[a-z]{2,})[\w\?\/\=\.\#]*/g
+// Source: https://gist.github.com/dperini/729294
+const URL_REGEX = /(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[\/?#]\S*)?/i
 
 export const typeSafeEq = (x, y) => x.toString() === y.toString()
 export const orStr = e => e || ''
