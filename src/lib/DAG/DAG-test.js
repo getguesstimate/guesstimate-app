f146123f50c3b9050989c45adc9880c7104925df
Matthew McDermott <mattmcdermott8@gmail.com>
2016-10-11 12:11:22 -0700

    Cleaned up tests a bit.

--- src/lib/DAG/DAG-test.js
+++ src/lib/DAG/DAG-test.js
@@ -11,49 +11,20 @@ import {
 
 describe('DAG library', () => {
   const layeredGraphNodes = [
-    {
-      id: 'A',
-      inputs: [],
-    }, {
-      id: 'B',
-      inputs: ['A'],
-    }, {
-      id: 'C',
-      inputs: ['A'],
-    }, {
-      id: 'D',
-      inputs: ['B'],
-    }, {
-      id: 'E',
-      inputs: ['B'],
-    }, {
-      id: 'F',
-      inputs: ['C'],
-    }, {
-      id: 'G',
-      inputs: ['F', 'D'],
-    }, {
-      id: 'H',
-      inputs: ['I'],
-    }, {
-      id: 'I',
-      inputs: ['H', 'A'],
-    }, {
-      id: 'J',
-      inputs: ['G', 'I'],
-    }, {
-      id: 'K',
-      inputs: [],
-    }, {
-      id: 'L',
-      inputs: ['K'],
-    }, {
-      id: 'M',
-      inputs: ['K'],
-    }, {
-      id: 'N',
-      inputs: ['M', 'L'],
-    }
+    {id: 'A', inputs: []},
+    {id: 'B', inputs: ['A']},
+    {id: 'C', inputs: ['A']},
+    {id: 'D', inputs: ['B']},
+    {id: 'E', inputs: ['B']},
+    {id: 'F', inputs: ['C']},
+    {id: 'G', inputs: ['F', 'D']},
+    {id: 'H', inputs: ['I']},
+    {id: 'I', inputs: ['H', 'A']},
+    {id: 'J', inputs: ['G', 'I']},
+    {id: 'K', inputs: []},
+    {id: 'L', inputs: ['K']},
+    {id: 'M', inputs: ['K']},
+    {id: 'N', inputs: ['M', 'L']},
   ]
   describe('getAncestors', () => {
     it ('Correctly assigns nodes their relations', () => {
@@ -75,49 +46,20 @@ describe('DAG library', () => {
   })
 
   const withCycles = [
-    {
-      id: 'A',
-      inputs: ['G'],
-    }, {
-      id: 'B',
-      inputs: ['A'],
-    }, {
-      id: 'C',
-      inputs: ['B'],
-    }, {
-      id: 'D',
-      inputs: ['C'],
-    }, {
-      id: 'E',
-      inputs: ['D', 'K'],
-    }, {
-      id: 'F',
-      inputs: ['E'],
-    }, {
-      id: 'G',
-      inputs: ['F'],
-    }, {
-      id: 'H',
-      inputs: ['K'],
-    }, {
-      id: 'I',
-      inputs: ['H'],
-    }, {
-      id: 'J',
-      inputs: ['I'],
-    }, {
-      id: 'K',
-      inputs: ['J'],
-    }, {
-      id: '1',
-      inputs: [],
-    }, {
-      id: '2',
-      inputs: ['1'],
-    }, {
-      id: '3',
-      inputs: ['1'],
-    }
+    {id: 'A', inputs: ['G']},
+    {id: 'B', inputs: ['A']},
+    {id: 'C', inputs: ['B']},
+    {id: 'D', inputs: ['C']},
+    {id: 'E', inputs: ['D', 'K']},
+    {id: 'F', inputs: ['E']},
+    {id: 'G', inputs: ['F']},
+    {id: 'H', inputs: ['K']},
+    {id: 'I', inputs: ['H']},
+    {id: 'J', inputs: ['I']},
+    {id: 'K', inputs: ['J']},
+    {id: '1', inputs: []},
+    {id: '2', inputs: ['1']},
+    {id: '3', inputs: ['1']}
   ]
   describe('getCycleSets', () => {
     const ancestors = getAncestors(withCycles)
@@ -125,55 +67,24 @@ describe('DAG library', () => {
 
     it ('Correctly extracts cycles', () => {
       expect(acyclicNodes).to.deep.have.members([
-        {
-          id: '1',
-          inputs: [],
-        }, {
-          id: '2',
-          inputs: ['1'],
-        }, {
-          id: '3',
-          inputs: ['1'],
-        }
+        {id: '1',     inputs: []},
+        {id: '2',     inputs: ['1']},
+        {id: '3',     inputs: ['1']},
       ])
       expect(cycleSets).to.deep.have.members([
         [
-          {
-            id: 'A',
-            inputs: ['G'],
-          }, {
-            id: 'B',
-            inputs: ['A'],
-          }, {
-            id: 'C',
-            inputs: ['B'],
-          }, {
-            id: 'D',
-            inputs: ['C'],
-          }, {
-            id: 'E',
-            inputs: ['D', 'K'],
-          }, {
-            id: 'F',
-            inputs: ['E'],
-          }, {
-            id: 'G',
-            inputs: ['F'],
-          }
+          {id: 'A', inputs: ['G']},
+          {id: 'B', inputs: ['A']},
+          {id: 'C', inputs: ['B']},
+          {id: 'D', inputs: ['C']},
+          {id: 'E', inputs: ['D', 'K']},
+          {id: 'F', inputs: ['E']},
+          {id: 'G', inputs: ['F']}
         ], [
-          {
-            id: 'H',
-            inputs: ['K'],
-          }, {
-            id: 'I',
-            inputs: ['H'],
-          }, {
-            id: 'J',
-            inputs: ['I'],
-          }, {
-            id: 'K',
-            inputs: ['J'],
-          }
+          {id: 'H', inputs: ['K']},
+          {id: 'I', inputs: ['H']},
+          {id: 'J', inputs: ['I']},
+          {id: 'K', inputs: ['J']}
         ],
       ])
     })
@@ -187,28 +98,13 @@ describe('DAG library', () => {
           inputs: ['K'],
           dependants: [],
           nodes: [
-            {
-              id: 'A',
-              inputs: ['G'],
-            }, {
-              id: 'B',
-              inputs: ['A'],
-            }, {
-              id: 'C',
-              inputs: ['B'],
-            }, {
-              id: 'D',
-              inputs: ['C'],
-            }, {
-              id: 'E',
-              inputs: ['D', 'K'],
-            }, {
-              id: 'F',
-              inputs: ['E'],
-            }, {
-              id: 'G',
-              inputs: ['F'],
-            }
+            {id: 'A', inputs: ['G']},
+            {id: 'B', inputs: ['A']},
+            {id: 'C', inputs: ['B']},
+            {id: 'D', inputs: ['C']},
+            {id: 'E', inputs: ['D', 'K']},
+            {id: 'F', inputs: ['E']},
+            {id: 'G', inputs: ['F']}
           ],
         }, {
           id: null,
@@ -216,19 +112,10 @@ describe('DAG library', () => {
           inputs: [],
           dependants: [],
           nodes: [
-            {
-              id: 'H',
-              inputs: ['K'],
-            }, {
-              id: 'I',
-              inputs: ['H'],
-            }, {
-              id: 'J',
-              inputs: ['I'],
-            }, {
-              id: 'K',
-              inputs: ['J'],
-            }
+            {id: 'H', inputs: ['K']},
+            {id: 'I', inputs: ['H']},
+            {id: 'J', inputs: ['I']},
+            {id: 'K', inputs: ['J']}
           ]
         },
       ])
@@ -237,22 +124,11 @@ describe('DAG library', () => {
 
   describe('getMissingInputs', () => {
     const nodes = [
-      {
-        id: '1',
-        inputs: [],
-      }, {
-        id: '2',
-        inputs: ['missing', 'missing'],
-      }, {
-        id: '3',
-        inputs: ['1', '2'],
-      }, {
-        id: '4',
-        inputs: ['2', '3', 'missing'],
-      }, {
-        id: '5',
-        inputs: ['3', '4', 'gone', 'not'],
-      }
+      {id: '1', inputs: []},
+      {id: '2', inputs: ['missing', 'missing']},
+      {id: '3', inputs: ['1', '2']},
+      {id: '4', inputs: ['2', '3', 'missing']},
+      {id: '5', inputs: ['3', '4', 'gone', 'not']}
     ]
 
     it('extracts missing inputs', () => {
@@ -263,126 +139,52 @@ describe('DAG library', () => {
 
   describe('separateIntoHeightSets', () => {
     const nodesToSeparate = [
-      {
-        id: '0',
-        inputs: [],
-      }, {
-        id: '1',
-        inputs: [],
-      }, {
-        id: '2',
-        inputs: ['1'],
-      }, {
-        id: '3',
-        inputs: ['1'],
-      }, {
-        id: null,
-        isCycle: true,
-        inputs: ['3'],
-        nodes: [{id: '4', inputs: ['3', '5']}, {id: '5', inputs: ['4']}],
-      }, {
-        id: '6',
-        inputs: ['3'],
-      }, {
-        id: '7',
-        inputs: ['1', '6'],
-      }, {
-        id: '8',
-        inputs: ['5', '4', 'missing'],
-      }
+      {id: '0', inputs: []},
+      {id: '1', inputs: []},
+      {id: '2', inputs: ['1']},
+      {id: '3', inputs: ['1']},
+      {id: null, isCycle: true, inputs: ['3'], nodes: [{id: '4', inputs: ['3', '5']}, {id: '5', inputs: ['4']}]},
+      {id: '6', inputs: ['3']},
+      {id: '7', inputs: ['1', '6']},
+      {id: '8', inputs: ['5', '4', 'missing']},
     ]
 
     it('separates into proper height sets', () => {
       const heightSets = separateIntoHeightSets(nodesToSeparate)
       expect(heightSets).to.deep.equal([
+        [{id: '0', inputs: []}, {id: '1', inputs: []}],
+        [{id: '2', inputs: ['1']}, {id: '3', inputs: ['1']}],
         [
-          {
-            id: '0',
-            inputs: [],
-          }, {
-            id: '1',
-            inputs: [],
-          }
-        ], [
-          {
-            id: '2',
-            inputs: ['1'],
-          }, {
-            id: '3',
-            inputs: ['1'],
-          }
-        ], [
-          {
-            id: null,
-            isCycle: true,
-            inputs: ['3'],
-            nodes: [{id: '4', inputs: ['3', '5']}, {id: '5', inputs: ['4']}],
-          }, {
-            id: '6',
-            inputs: ['3'],
-          }
-        ], [
-          {
-            id: '7',
-            inputs: ['1', '6'],
-          }, {
-            id: '8',
-            inputs: ['5', '4', 'missing'],
-          }
-        ]
+          {id: null, isCycle: true, inputs: ['3'], nodes: [{id: '4', inputs: ['3', '5']}, {id: '5', inputs: ['4']}]},
+          {id: '6', inputs: ['3']}
+        ],
+        [{id: '7', inputs: ['1', '6']}, {id: '8', inputs: ['5', '4', 'missing']}],
       ])
     })
   })
 
   describe('separateIntoDisconnectedComponents', () => {
     const nodes = [
-      {
-        id: '1',
-        inputs: [],
-      }, {
-        id: '2',
-        inputs: ['1'],
-      }, {
-        id: '3',
-        inputs: ['1'],
-      }, {
-        id: '4',
-        inputs: [],
-      }, {
-        id: '5',
-        inputs: ['4'],
-      }, {
-        id: '6',
-        inputs: ['4', '5'],
-      }
+      {id: '1', inputs: []},
+      {id: '2', inputs: ['1']},
+      {id: '3', inputs: ['1']},
+      {id: '4', inputs: []},
+      {id: '5', inputs: ['4']},
+      {id: '6', inputs: ['4', '5']},
     ]
 
     const ancestors = getAncestors(nodes)
     const components = separateIntoDisconnectedComponents(nodes, ancestors)
     expect(components).to.have.length(2)
     expect(components[0]).to.deep.have.members([
-      {
-        id: '1',
-        inputs: [],
-      }, {
-        id: '2',
-        inputs: ['1'],
-      }, {
-        id: '3',
-        inputs: ['1'],
-      }
+      {id: '1', inputs: []},
+      {id: '2', inputs: ['1']},
+      {id: '3', inputs: ['1']},
     ])
     expect(components[1]).to.deep.have.members([
-      {
-        id: '4',
-        inputs: [],
-      }, {
-        id: '5',
-        inputs: ['4'],
-      }, {
-        id: '6',
-        inputs: ['4', '5'],
-      }
+      {id: '4', inputs: []},
+      {id: '5', inputs: ['4']},
+      {id: '6', inputs: ['4', '5']},
     ])
   })
 })
