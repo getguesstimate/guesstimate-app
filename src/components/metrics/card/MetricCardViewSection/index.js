010f101db948ae5ad0ee3b91c603cf462d24789d
Matthew McDermott <mattmcdermott8@gmail.com>
2016-11-16 17:02:52 -0800

    set up flash triggering (function dependence); no flash css or timed state yet.

--- src/components/metrics/card/MetricCardViewSection/index.js
+++ src/components/metrics/card/MetricCardViewSection/index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 
+import $ from 'jquery'
 import Icon from 'react-fa'
 
 import Histogram from 'gComponents/simulations/histogram/index'
@@ -39,6 +40,20 @@ const ErrorSection = ({errors, padTop, shouldShowErrorText, messageToDisplay}) =
 )
 
 export class MetricCardViewSection extends Component {
+  componentDidUpdate() {
+    const {canvasState: {metricClickMode}, inSelectedCell} = this.props
+    const selfFunctionSelected = (metricClickMode === 'FUNCTION_INPUT_SELECT') && inSelectedCell
+    if (selfFunctionSelected) {
+      $(window).on('functionMetricClicked', this.flash.bind(this))
+    } else {
+      $(window).off('functionMetricClicked')
+    }
+  }
+
+  flash() {
+    console.log('woo flash triggered')
+  }
+
   hasContent() { return _.result(this.refs, 'name.hasContent') }
   focusName() { _.result(this.refs, 'name.focus') }
 
