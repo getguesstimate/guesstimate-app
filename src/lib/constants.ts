9dd6fcf51365d6c25a105c5cd6f799d85bd8a460
Vyacheslav Matyukhin <me@berekuk.ru>
2023-01-21 22:39:08 -0600

    remove more packages; update constants for Vercel

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,9 @@
-export const __DEV__ = process.env.NODE_ENV === "development";
-export const __API_ENV__ = process.env.API_ENV || "development";
+export const __DEV__ =
+  process.env.NODE_ENV === "development" ||
+  process.env.NEXT_PUBLIC_VERCEL_ENV === "development";
 
-export const BASE_URL = __DEV__
-  ? "http://localhost:3000"
-  : "https://www.getguesstimate.com";
+export const __API_ENV__ = process.env.NEXT_PUBLIC_VERCEL_ENV || "development";
+
+export const BASE_URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+  : "http://localhost:3000";
