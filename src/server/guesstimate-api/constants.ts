e450b670c48a7195db021051033f372066475161
Vyacheslav Matyukhin <me@berekuk.ru>
2024-03-03 18:32:40 -0600

    prod backend endpoint

--- src/server/guesstimate-api/constants.ts
+++ src/server/guesstimate-api/constants.ts
@@ -3,4 +3,4 @@ import { __API_ENV__ } from "~/lib/constants";
 export const rootUrl =
   __API_ENV__ === "development"
     ? "http://localhost:4000"
-    : "https://guesstimate.herokuapp.com";
+    : "https://api.guesstimate.com";
