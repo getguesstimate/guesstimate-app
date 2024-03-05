b595358e0c9539555dcbd2f0ba0ad2fca002e8ed
Vyacheslav Matyukhin <me@berekuk.ru>
2024-03-05 15:20:07 -0600

    remove check, fix tests

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -2,8 +2,5 @@ export const DEBUG = process.env.NEXT_PUBLIC_LOG_LEVEL === "debug";
 
 // `?? ""` is to guarantee that value is a string, for correct types
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? "";
-if (!API_BASE_URL) {
-  throw new Error("NEXT_PUBLIC_API_BASE_URL is not set");
-}
 
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
